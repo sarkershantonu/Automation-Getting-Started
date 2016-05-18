88441bc8993bb8cde3f56496ff8468030b64690d
sarkershantonu <shantonu_oxford@yahoo.com>
2016-05-18 08:03:55 +0200

    refactored

--- Junit-cucumber/src/main/java/org/pages/HomePage.java
+++ Junit-cucumber/src/main/java/org/pages/HomePage.java
@@ -8,10 +8,9 @@ import org.openqa.selenium.WebDriver;
  * Created by shantonu on 5/16/16.
  */
 public class HomePage extends PageBase {
+    private @Getter SearchPanel search;
     public HomePage(WebDriver aDriver) {
         super(aDriver);
         search = new SearchPanel(driver);
     }
-
-    private @Getter SearchPanel search;
 }
