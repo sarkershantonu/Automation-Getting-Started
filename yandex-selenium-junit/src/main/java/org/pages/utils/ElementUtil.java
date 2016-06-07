0a9303ef53fb37ec96ae13283611f91b776031c4
sarkershantonu <shantonu_oxford@yahoo.com>
2016-06-07 20:21:49 +0200

    Base Util class added

--- yandex-selenium-junit/src/main/java/org/pages/utils/ElementUtil.java
+++ yandex-selenium-junit/src/main/java/org/pages/utils/ElementUtil.java
@@ -1,9 +1,14 @@
 package org.pages.utils;
 
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by shantonu on 6/6/16.
  * this class is responsible for all utilitie for yandex element based pages.
  */
-public class ElementUtil {
+public class ElementUtil extends UtilBase {
 
+    public ElementUtil(WebDriver aDriver) {
+        super(aDriver);
+    }
 }
