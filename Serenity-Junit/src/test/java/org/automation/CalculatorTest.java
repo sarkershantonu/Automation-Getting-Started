288d2d586dfeae09ce55fe0f93601dbaf54de341
sarkershantonu <shantonu_oxford@yahoo.com>
2016-06-26 13:25:06 +0200

    add new items

--- Serenity-Junit/src/test/java/org/automation/CalculatorTest.java
+++ Serenity-Junit/src/test/java/org/automation/CalculatorTest.java
@@ -1,11 +1,17 @@
 package org.automation;
 
 
+import net.serenitybdd.core.Serenity;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Issue;
+import net.thucydides.core.annotations.Issues;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 
+@RunWith(SerenityRunner.class)
 public class CalculatorTest {
 
     private Calculator calculator;
@@ -20,6 +26,8 @@ public class CalculatorTest {
 
 
     @Test
+    @Issue("#157")
+    @Issues({"#156","#155"})
     public void testAdd() throws Throwable {
 
         Assert.assertEquals(5.0,calculator.add(3.0, 2.0),0.01);
