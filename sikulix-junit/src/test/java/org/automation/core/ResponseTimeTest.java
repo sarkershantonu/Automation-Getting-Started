42acf512f7a3d7df54e1ab8f419d17a28975047f
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-06 23:50:25 +0200

    timeout template

--- sikulix-junit/src/test/java/org/automation/core/ResponseTimeTest.java
+++ sikulix-junit/src/test/java/org/automation/core/ResponseTimeTest.java
@@ -1,9 +1,14 @@
 package org.automation.core;
 
+import org.junit.Rule;
+import org.junit.rules.Timeout;
+
 /**
  * Created by shantonu on 7/6/16.
  */
 public abstract class ResponseTimeTest extends TestBase{
 
-    
+    @Rule
+    public Timeout globalTimeOut = Timeout.seconds(2);
+
 }
