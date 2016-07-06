2e7de414cfe85174397a46639b7fb60b17d81394
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-07 00:01:46 +0200

    initial doc

---
+++ sikulix-junit/src/main/java/org/automation/api/ScreenBase.java
@@ -0,0 +1,10 @@
+package org.automation.api;
+
+import org.sikuli.script.Screen;
+
+/**
+ * Created by shantonu on 7/6/16.
+ */
+public class ScreenBase {
+    private static Screen screen = new Screen();
+}
