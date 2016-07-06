56c93adcb8e998117f1b93f8353d25a75841c6ff
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-06 23:43:28 +0200

    complete regression

---
+++ sikulix-junit/src/test/java/org/automation/Regression.java
@@ -0,0 +1,12 @@
+package org.automation;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by shantonu on 7/6/16.
+ */
+
+@RunWith(Suite.class)
+public class Regression {
+}
