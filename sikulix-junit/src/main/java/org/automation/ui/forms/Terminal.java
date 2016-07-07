c4011a383bc688915907db3a8fd7493867639985
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-07 02:34:20 +0200

    terminal

---
+++ sikulix-junit/src/main/java/org/automation/ui/forms/Terminal.java
@@ -0,0 +1,12 @@
+package org.automation.ui.forms;
+
+import org.automation.ui.UiBase;
+
+/**
+ * Created by shantonu on 7/7/16.
+ */
+public class Terminal extends UiBase {
+    public class Keyboard extends UiBase{}
+    public class Bell extends UiBase{}
+    public class Features extends UiBase{}
+}
