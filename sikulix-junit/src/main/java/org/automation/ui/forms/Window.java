5133d261ddaee3fe1783d648783aab629195969b
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-07 02:34:34 +0200

    windows

---
+++ sikulix-junit/src/main/java/org/automation/ui/forms/Window.java
@@ -0,0 +1,17 @@
+package org.automation.ui.forms;
+
+import org.automation.ui.UiBase;
+
+/**
+ * Created by shantonu on 7/7/16.
+ */
+public class Window extends UiBase{
+
+    public class Appearance extends UiBase{}
+    public class Behavior extends UiBase{}
+    public class Transaction extends UiBase{}
+    public class Selection extends UiBase{}
+    public class Colours extends UiBase{}
+    public class Fonts extends UiBase{}
+
+}
