48139bfa0d19fbf9bb19676cc22242313bc9c6d8
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-07 02:59:52 +0200

    util added

--- sikulix-junit/src/main/java/org/automation/utils/MatcherUtil.java
+++ sikulix-junit/src/main/java/org/automation/utils/MatcherUtil.java
@@ -1,7 +1,24 @@
 package org.automation.utils;
 
+import org.automation.api.ScreenBase;
+import org.sikuli.script.Image;
+
 /**
  * Created by shantonu on 7/7/16.
  */
-public class MatcherUtil {
+public class MatcherUtil extends ScreenBase{
+    public static boolean isImagePresent(String path){
+        return false; //// TODO: 7/7/16
+    }
+
+    public static boolean isTextPresent(String target){
+        return false; //// TODO: 7/7/16
+    }
+
+    public static boolean areEqual(String subjectImagePath, String targetImagePath){
+        return false; //// TODO: 7/7/16
+    }
+    public static boolean areEqual(Image subjectImagePath, Image targetImagePath){
+        return false; //// TODO: 7/7/16  
+    }
 }
