2f35667e1525e3b5ad7c78027d58def0be1f8aed
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-07 02:15:50 +0200

    session added

---
+++ sikulix-junit/src/main/java/org/automation/ui/common/LeftPanel.java
@@ -0,0 +1,9 @@
+package org.automation.ui.common;
+
+import org.automation.ui.UiBase;
+
+/**
+ * Created by shantonu on 7/7/16.
+ */
+public class LeftPanel extends UiBase {
+}
---
+++ sikulix-junit/src/main/java/org/automation/ui/components/Session.java
@@ -0,0 +1,13 @@
+package org.automation.ui.components;
+
+import org.automation.ui.UiBase;
+
+/**
+ * Created by shantonu on 7/7/16.
+ */
+public class Session extends UiBase {
+
+    public class Logging extends UiBase{
+    }
+}
+
