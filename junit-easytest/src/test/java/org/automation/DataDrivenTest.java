6e3c2a31e585cd4a104e4acd9c0db0694cf12682
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-15 23:27:39 +0200

    initial commit

--- junit-easytest/src/test/java/org/automation/DataDrivenTest.java
+++ junit-easytest/src/test/java/org/automation/DataDrivenTest.java
@@ -1,8 +1,13 @@
 package org.automation;
 
+import org.easetech.easytest.annotation.Report;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
+import org.junit.runner.RunWith;
+
 /**
  * Created by shantonu on 7/15/16.
  */
-
-public class DataDrivenTest {
+@RunWith(DataDrivenTestRunner.class)
+@Report(outputLocation = "file:Reports", outputFormats = Report.EXPORT_FORMAT.PDF, reportTypes = {Report.REPORT_TYPE.METHOD_DURATION, Report.REPORT_TYPE.DEFAULT})
+public abstract class DataDrivenTest {
 }
