37140900b18d5f4edff680742f6128f8163a24c0
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-15 23:40:48 +0200

    small test

--- junit-easytest/src/test/java/org/automation/UnitTests/CalculatorTest.java
+++ junit-easytest/src/test/java/org/automation/UnitTests/CalculatorTest.java
@@ -1,7 +1,24 @@
 package org.automation.UnitTests;
 
+import org.automation.Calculator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
 /**
  * Created by shantonu on 7/15/16.
  */
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void init(){
+        calculator = new Calculator();
+    }
+    @Test
+    public void testAdd(){
+        Assert.assertEquals(25.0, calculator.add(10.5,14.5),0.1);
+    }
+
 }
