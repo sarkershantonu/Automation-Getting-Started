ab3c1b63fb09cb0b83bb4242e6fe1655bde0cbf8
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-16 02:39:13 +0200

    custom data converter

--- junit-easytest/src/test/java/org/automation/UnitTests/TestCustomObjectConverter.java
+++ junit-easytest/src/test/java/org/automation/UnitTests/TestCustomObjectConverter.java
@@ -1,8 +1,12 @@
 package org.automation.UnitTests;
 
 import org.automation.Calculator;
+import org.automation.MyDataConverter;
+import org.easetech.easytest.converter.ConverterManager;
+import org.easetech.easytest.converter.UserDefinedConverter;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
@@ -11,9 +15,13 @@ import org.junit.runners.BlockJUnit4ClassRunner;
  * Created by shantonu on 7/16/16.
  */
 
-public class WithRegularRunner {
+public class TestCustomObjectConverter {
     private Calculator calculator;
 
+    @BeforeClass
+    public static void initClass(){
+        ConverterManager.registerConverter(MyDataConverter.class);
+    }
 
     @Before
     public void init(){
