f2ca08523a89cadbe5d3b1c5669791183cbe13ec
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-16 03:30:47 +0200

    parallel suit

--- junit-easytest/src/test/java/org/automation/UnitTests/CalculatorTest.java
+++ junit-easytest/src/test/java/org/automation/UnitTests/CalculatorTest.java
@@ -10,30 +10,40 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.easetech.easytest.annotation.Display;
+import org.easetech.easytest.annotation.Duration;
 
 /**
  * Created by shantonu on 7/15/16.
  */
 
-public class CalculatorTest extends DataDrivenTest{
+
+public class CalculatorTest extends DataDrivenTest {
 
     @Test
-    public void testAdd(){
-        Assert.assertEquals(25.0, calculator.add(10.5,14.5),0.1);
+    public void testAdd() {
+        Assert.assertEquals(25.0, calculator.add(10.5, 14.5), 0.1);
     }
 
     @Test
     @DataLoader(filePaths = "calculator.csv", loaderType = LoaderType.CSV)
+    @Duration(forClass = CalculatorTest.class, timeInMillis = 100)//duration condition does not work
+    public void testAddFromCSV(@Param(name = "a") Double a, @Param(name = "b") Double b, @Param(name = "expected") Double expected) {
 
-    public void testAddFromCSV(@Param(name = "a") Double a, @Param(name = "b")Double b, @Param(name = "expected")Double expected){
-        Assert.assertEquals(expected, calculator.add(a,b),0.1);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
     }
 
     @Test
     @DataLoader(filePaths = "calculator.xls", loaderType = LoaderType.EXCEL)
 
-    public void testAddFromExcel(@Param(name = "a") Double a, @Param(name = "b")Double b, @Param(name = "expected")Double expected){
-        Assert.assertEquals(expected, calculator.add(a,b),0.1);
+
+    public void testAddFromExcel(@Param(name = "a") Double a, @Param(name = "b") Double b, @Param(name = "expected") Double expected) {
+        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
+
     }
 
 }
--- junit-easytest/src/test/java/org/automation/suits/ParallelSuit.java
+++ junit-easytest/src/test/java/org/automation/suits/ParallelSuit.java
@@ -3,5 +3,14 @@ package org.automation.suits;
 /**
  * Created by shantonu on 7/16/16.
  */
+import org.automation.UnitTests.CalculatorTest;
+import org.automation.UnitTests.MultipleDataLoader;
+import org.easetech.easytest.annotation.ParallelSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@ParallelSuite(threads = 3)
+@Suite.SuiteClasses({CalculatorTest.class, MultipleDataLoader.class})
 public class ParallelSuit {
 }
