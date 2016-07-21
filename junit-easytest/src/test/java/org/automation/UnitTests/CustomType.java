20eeb873e940d05742d0a885bbec25fb067522d9
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-21 23:14:29 +0200

    Custom Type added for test

---
+++ junit-easytest/src/test/java/org/automation/UnitTests/CustomType.java
@@ -0,0 +1,12 @@
+package org.automation.UnitTests;
+
+import org.automation.policy.LocalTestPolicy;
+import org.easetech.easytest.annotation.TestPolicy;
+
+/**
+ * Created by shantonu on 7/21/16.
+ */
+@TestPolicy(LocalTestPolicy.class)
+public class CustomType {
+
+}
