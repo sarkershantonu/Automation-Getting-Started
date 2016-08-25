daa75a53d7a8842890fe47af0b7baa06980f59d2
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-26 00:15:26 +0200

    Performance report example

---
+++ junit-easytest/src/test/java/org/automation/reports/PerformanceReportExample.java
@@ -0,0 +1,32 @@
+package org.automation.reports;
+
+import org.automation.core.DataDrivenTest;
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.annotation.Report;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by shantonu on 8/26/16.
+ */
+
+@RunWith(DataDrivenTestRunner.class)
+@Report(reportTypes = Report.REPORT_TYPE.METHOD_DURATION,outputLocation = "file:TestReports")// report type default is assertion results & multi format allowed
+public class PerformanceReportExample extends DataDrivenTest {
+
+    @Test
+    @DataLoader(filePaths = "calculator.xls")
+    public void testAddFromExcel(@Param(name = "a") Double a, @Param(name = "b") Double b, @Param(name = "expected") Double expected) {
+        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
+    }
+
+    @Test
+    @DataLoader(filePaths = "calculator2.xls")
+    public void testAdd(@Param(name = "a") Double a, @Param(name = "b") Double b, @Param(name = "expected") Double expected) {
+        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
+    }
+
+}
