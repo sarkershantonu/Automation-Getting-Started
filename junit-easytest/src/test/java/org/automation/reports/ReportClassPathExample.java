10306f0f3720fcea6ab51390a1031dbb9714a07e
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-25 23:58:45 +0200

    class path report

---
+++ junit-easytest/src/test/java/org/automation/reports/ReportClassPathExample.java
@@ -0,0 +1,29 @@
+package org.automation.reports;
+
+import org.automation.core.DataDrivenTest;
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.loader.LoaderType;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by shantonu on 8/23/16.
+ */
+@RunWith(DataDrivenTestRunner.class)
+
+public class ReportClassPathExample extends DataDrivenTest{
+
+    @Test
+    @DataLoader(filePaths = "calculator.xls")
+    public void testAddFromExcel(@Param(name = "a") Double a, @Param(name = "b") Double b, @Param(name = "expected") Double expected) {
+        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
+    }
+    @Test
+    @DataLoader(filePaths = "calculator2.xls")
+    public void testAdd(@Param(name = "a") Double a, @Param(name = "b") Double b, @Param(name = "expected") Double expected) {
+        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
+    }
+}
