3852065c69a4f2465fd42090b5dc68d57c0b74f4
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-25 23:54:17 +0200

    loader type CSV

--- junit-easytest/src/test/java/org/automation/UnitTests/ReportingExamples.java
+++ junit-easytest/src/test/java/org/automation/UnitTests/ReportingExamples.java
@@ -4,12 +4,15 @@ import org.automation.core.DataDrivenTest;
 import org.easetech.easytest.annotation.DataLoader;
 import org.easetech.easytest.annotation.Param;
 import org.easetech.easytest.loader.LoaderType;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by shantonu on 8/23/16.
  */
+@RunWith(DataDrivenTestRunner.class)
 @DataLoader(filePaths = "calculator.csv", loaderType = LoaderType.CSV)
 public class ReportingExamples extends DataDrivenTest{
 
@@ -21,22 +24,5 @@ public class ReportingExamples extends DataDrivenTest{
         Assert.assertEquals(expected, calculator.add(a, b), 0.1);
     }
 
-    @Test
 
-    public void testAddFromExcel(@Param(name = "a") Double a, @Param(name = "b") Double b, @Param(name = "expected") Double expected) {
-        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
-    }
-    @Test
-
-    public void testAddFromXML(@Param(name = "a") Double a, @Param(name = "b") Double b, @Param(name = "expected") Double expected) {
-        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
-
-    }
-    @Test
-
-    public String testAddXMLOutput(@Param(name = "a") Double a, @Param(name = "b") Double b, @Param(name = "expected") Double expected) {
-        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
-        return "success";
-
-    }
 }
