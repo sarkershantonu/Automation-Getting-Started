ae7688189350dabee0e3c9a4b7b3a068e23915a7
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-26 08:51:19 +0200

    Parallel Test Execution Example

--- junit-easytest/src/test/java/org/automation/examples/ParallelTestExample.java
+++ junit-easytest/src/test/java/org/automation/examples/ParallelTestExample.java
@@ -1,32 +1,22 @@
 package org.automation.examples;
 
 import org.automation.core.DataDrivenTest;
+import org.automation.core.DataDrivenTest_withDefaultAnnotation;
 import org.easetech.easytest.annotation.*;
 import org.easetech.easytest.loader.LoaderType;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by shantonu on 7/16/16.
  */
-@Parallel(threads = 1)//this is very fragile
-public class ParallelTestExample extends DataDrivenTest{
-    @Test
-    public void testAdd(){
-        Assert.assertEquals(25.0, calculator.add(10.5,14.5),0.1);
-    }
-
-    @Test
-    @DataLoader(filePaths = "calculator.csv", loaderType = LoaderType.CSV)
-    @Display(fields = "expected")
-    public void testAddFromCSV(@Param(name = "a") Double a, @Param(name = "b")Double b, @Param(name = "expected")Double expected){
-        Assert.assertEquals(expected, calculator.add(a,b),0.1);
-    }
+@Parallel(threads = 5)//this is very fragile
+public class ParallelTestExample extends DataDrivenTest_withDefaultAnnotation {
 
     @Test
     @DataLoader(filePaths = "calculator.xls", loaderType = LoaderType.EXCEL)
-    @Display(fields = "a")
-    @Repeat(times = 2)
     public void testAddFromExcel(@Param(name = "a") Double a, @Param(name = "b")Double b, @Param(name = "expected")Double expected){
         Assert.assertEquals(expected, calculator.add(a,b),0.1);
     }
