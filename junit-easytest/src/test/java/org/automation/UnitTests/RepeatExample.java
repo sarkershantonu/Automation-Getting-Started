489b1c8f98601b145ca1b51e187accf43210cf8c
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-26 08:29:22 +0200

    Repeat Example

--- junit-easytest/src/test/java/org/automation/UnitTests/RepeatExample.java
+++ junit-easytest/src/test/java/org/automation/UnitTests/RepeatExample.java
@@ -1,7 +1,30 @@
 package org.automation.UnitTests;
 
+import org.automation.core.DataDrivenTest;
+import org.automation.policy.PolicyExample;
+import org.easetech.easytest.annotation.Duration;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.annotation.Repeat;
+import org.easetech.easytest.annotation.TestPolicy;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 /**
  * Created by shantonu on 8/26/16.
  */
-public class RepeatExample {
+
+@RunWith(DataDrivenTestRunner.class)
+@TestPolicy(PolicyExample.class)
+public class RepeatExample extends DataDrivenTest {
+
+    @Test
+    @Repeat(times = 5)
+    public void testAddFromExcel(@Param(name = "a") Double a,
+                                 @Param(name = "b") Double b,
+                                 @Param(name = "expected") Double expected) {
+        Assert.assertEquals(expected, calculator.add(a, b), 0.1);
+    }
+
 }
