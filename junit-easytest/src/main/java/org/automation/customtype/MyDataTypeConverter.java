5ec3bb029d275b31e73b1a5ff4c8de0e79f563ce
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-27 23:56:46 +0200

    Custom Type Converter added

---
+++ junit-easytest/src/main/java/org/automation/customtype/MyDataTypeConverter.java
@@ -0,0 +1,14 @@
+package org.automation.customtype;
+
+import org.easetech.easytest.converter.AbstractConverter;
+
+import java.util.Map;
+
+/**
+ * Created by shantonu on 8/27/16.
+ */
+public class MyDataTypeConverter extends AbstractConverter<MyDataType> {
+    public MyDataType convert(Map<String, Object> convertFrom) {
+        return null;
+    }
+}
