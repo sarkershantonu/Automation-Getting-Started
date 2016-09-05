4a1536671286180d0602515f1cbc937163480ba7
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-09-05 13:43:21 +0200

    Screen base primary items added

--- sikulix-junit/src/main/java/org/automation/api/ScreenBase.java
+++ sikulix-junit/src/main/java/org/automation/api/ScreenBase.java
@@ -6,5 +6,34 @@ import org.sikuli.script.Screen;
  * Created by shantonu on 7/6/16.
  */
 public class ScreenBase {
-    protected static Screen screen = new Screen();
+   protected static Screen screen = new Screen();
+
+    protected void clickAndWait(String path) throws FindFailed {
+        screen.wait(path).click();
+        defaultWait();
+    }
+    protected void log(String message, Object... args){
+        SikuliUtil.log(0,this.getClass(),message, args);
+    }
+    public void wait(double time) {
+
+        SikuliUtil.waitFor(screen,time);
+    }
+    public void defaultWait(){
+        SikuliUtil.defaultWait(screen);
+    }
+    public boolean isVisable(String targetImagepath) throws FindFailed {
+        Match mactch = screen.exists(targetImagepath, ConfigUtils.waitForImage);
+        if(mactch==null){
+            return false;
+        }
+        return true;
+    }
+    protected void addImagePath(String path){
+        ImagePath.add(path);
+    }
+    protected void setImagePath(String path){
+        ImagePath.reset();
+        ImagePath.setBundlePath(path);
+    }
 }
