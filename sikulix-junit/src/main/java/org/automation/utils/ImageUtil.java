519801ac589dcdf3abcf18e65aaaaa05d94c2bcd
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-09-05 13:38:10 +0200

    ImageUtil planned

---
+++ sikulix-junit/src/main/java/org/automation/utils/ImageUtil.java
@@ -0,0 +1,14 @@
+import org.sikuli.script.Image;
+
+public class ImageUtil {
+    public static String getStringFromImage(Image image){
+        return null;//todo
+    }
+    public static Image getImageOf(String data){
+        return null;//todo
+    }
+
+    public static <T extends Number> Image getImageOf(T data){
+        return null; //todo
+    }
+}
