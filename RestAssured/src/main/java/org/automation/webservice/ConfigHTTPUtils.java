b07ad93a1ac69d746f5a78b5f055f3a8c6a18247
sarkershantonu <shantonu_oxford@yahoo.com>
2016-09-08 02:30:57 +0200

    initial plan for rest assured project

---
+++ RestAssured/src/main/java/org/automation/webservice/ConfigHTTPUtils.java
@@ -0,0 +1,14 @@
+package org.automation.webservice;
+
+import io.restassured.RestAssured;
+import io.restassured.config.RestAssuredConfig;
+
+/**
+ * Created by shantonu on 9/8/16.
+ */
+public class ConfigHTTPUtils {
+
+    public RestAssuredConfig defaultConfig(){
+        return null;
+    }
+}
---
+++ RestAssured/src/main/java/org/automation/webservice/ConfigHeadersUtils.java
@@ -0,0 +1,7 @@
+package org.automation.webservice;
+
+/**
+ * Created by shantonu on 9/8/16.
+ */
+public class ConfigHeadersUtils {
+}
