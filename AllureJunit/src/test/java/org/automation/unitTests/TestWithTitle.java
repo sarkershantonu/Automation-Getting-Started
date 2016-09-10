901dde4930e056075fc785e4ea8924570dc90210
sarkershantonu <shantonu_oxford@yahoo.com>
2016-09-10 22:41:52 +0200

    @Steps documented

--- AllureJunit/src/test/java/org/automation/unitTests/TestWithTitle.java
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithTitle.java
@@ -11,7 +11,7 @@ import ru.yandex.qatools.allure.annotations.Title;
 @Title("Class Title : Testing Calcularor")
 public class TestWithTitle extends TestingCalculator{
 
-    @Title("")
+    @Title("Method Title: {method}")
     public void addition(){
         Assert.assertEquals(25.0, aCalculator.add(10.0,15.0), 0.01);
     }
