4350049639cd209d3ad211c52d4fa5e0a071bc08
sarkershantonu <shantonu_oxford@yahoo.com>
2016-09-11 06:09:09 +0200

    Test With Stories

--- AllureJunit/src/test/java/org/automation/unitTests/TestWithFeatures.java
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithFeatures.java
@@ -1,9 +1,18 @@
 package org.automation.unitTests;
 
 import org.automation.core.TestingCalculator;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Features;
 
 /**
  * Created by shantonu on 9/11/16.
  */
 public class TestWithFeatures extends TestingCalculator{
+    @Test
+    @Features("Method Description : This will test addidtion")
+    public void testAddition() {
+        Assert.assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
+    }
 }
--- AllureJunit/src/test/java/org/automation/unitTests/TestWithStories.java
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithStories.java
@@ -1,9 +1,24 @@
 package org.automation.unitTests;
 
 import org.automation.core.TestingCalculator;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Stories;
 
 /**
  * Created by shantonu on 9/11/16.
  */
+@Stories(value = {"Class Story : Addition","Class Story : Substruction"})
 public class TestWithStories extends TestingCalculator {
+    @Test
+    @Stories(value = "Method Story : Addition")
+    public void testAddition() {
+        Assert.assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
+    }
+    @Test
+    @Stories("Method Story : Addition")
+    public void testAddition2() {
+        Assert.assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
+    }
 }
