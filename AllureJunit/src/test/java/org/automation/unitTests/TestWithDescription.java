e01547c5967af8277f2d3e7760adcbef05038758
sarkershantonu <shantonu_oxford@yahoo.com>
2016-09-11 06:05:47 +0200

    Test With Description

---
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithDescription.java
@@ -0,0 +1,20 @@
+package org.automation.unitTests;
+
+import org.automation.core.TestingCalculator;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Severity;
+import ru.yandex.qatools.allure.model.SeverityLevel;
+
+/**
+ * Created by shantonu on 9/11/16.
+ */
+@Description("Class Description : This will test calculator")
+public class TestWithDescription extends TestingCalculator {
+    @Test
+    @Description("Method Description : This will test addidtion")
+    public void testAddition() {
+        Assert.assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
+    }
+}
---
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithStories.java
@@ -0,0 +1,9 @@
+package org.automation.unitTests;
+
+import org.automation.core.TestingCalculator;
+
+/**
+ * Created by shantonu on 9/11/16.
+ */
+public class TestWithStories extends TestingCalculator {
+}
