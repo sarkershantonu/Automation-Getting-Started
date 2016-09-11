1752e3ef470481d02230bd7a787ed768ff30794a
sarkershantonu <shantonu_oxford@yahoo.com>
2016-09-11 06:42:17 +0200

    issues/issue example added with property

--- AllureJunit/src/test/java/org/automation/unitTests/TestWithIsseu.java
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithIsseu.java
@@ -9,17 +9,27 @@ import ru.yandex.qatools.allure.annotations.Issues;
 
 /**
  * Created by shantonu on 9/11/16.
+ * Notes :
+ * This issue tracker links can be managed by allure.properties
+ * allure.issues.tracker.pattern=
+ * All isseues & isseue are same location pointers /issues/$s
+ * This example will show you
+ * 1. All @issues are value with numbers for class level, M$ for method level
+ * 2. All @issue are value with prefix class/method
+ *
  */
 @Issues(value = {@Issue(value = "001"), @Issue(value = "002")})
-@Issue(value = "CLASS-1")
+@Issue(value = "ISSUE/CLASS-1")
 public class TestWithIsseu extends TestingCalculator {
     @Test
-    @Issue(value = "METHOD 1")
+    @Issue(value = "ISSUE/METHOD-1")
+    @Issues(value = {@Issue(value = "M001"), @Issue(value = "M002")})
     public void testAddition() {
         Assert.assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
     }
     @Test
-    @Issue(value = "METHOD 2")
+    @Issue(value = "ISSUE/METHOD-2")
+    @Issues(value = {@Issue(value = "M003"), @Issue(value = "M004")})
     public void testDiv(){
         Assert.assertEquals(5.0, aCalculator.div(250.0,50.0), 0.01);
     }
