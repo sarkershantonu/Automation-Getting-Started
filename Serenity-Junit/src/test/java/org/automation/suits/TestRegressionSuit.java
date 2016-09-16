5278c882cfbf5f0ed7f4f5a2a5b7dc97eb16ce4e
sarkershantonu <shantonu_oxford@yahoo.com>
2016-09-17 00:00:51 +0200

    suit class

--- Serenity-Junit/src/test/java/org/automation/suits/TestRegressionSuit.java
+++ Serenity-Junit/src/test/java/org/automation/suits/TestRegressionSuit.java
@@ -1,10 +1,12 @@
 package org.automation.suits;
 
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 /**
  * Created by shantonu on 9/16/16.
  */
 
+@RunWith(Suite.class)
 public class TestRegressionSuit {
 }
