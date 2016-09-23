3def7f0c4c611f17fbdd6c3accf3ea1e289cdfaa
Shantonu Sarker <sarkershantonu@users.noreply.github.com>
2016-09-23 19:24:45 +0200

    Sample story class

---
+++ Serenity-Junit/src/main/java/org/automation/pages/Application.java
@@ -0,0 +1,45 @@
+
+import net.thucydides.core.annotations.Feature;
+
+//sample StoryClass
+public class Application {
+
+    @Feature
+    public class Attributes{}
+    @Feature
+    public class Bau{}
+    @Feature
+    public class StaticDataAudit{}
+    @Feature
+    public class CacheRefresh{}
+    @Feature
+    public class Dashboard{}
+    @Feature
+    public class ExceptionDashboard{}
+    @Feature
+    public class Hierarchy{}
+    @Feature
+    public class Mscw{}
+    @Feature
+    public class Overrides{}
+    @Feature
+    public class Product{}
+    @Feature
+    public class Rules{}
+    @Feature
+    public class TemporaryClassification{}
+    @Feature
+    public class Hierarchies{}
+    @Feature
+    public class Pcs{}
+    @Feature
+    public class PcsWebservice{}
+    @Feature
+    public class GuiMaintenance{}
+    @Feature
+    public class GuiDashboard{}
+    @Feature
+    public class GUIExceptionDashboard{}
+    @Feature
+    public class PDQA {}
+}
