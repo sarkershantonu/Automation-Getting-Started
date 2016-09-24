92dc288acb55bff900c39c3e79d4f65d690813e8
sarkershantonu <shantonu_oxford@yahoo.com>
2016-09-24 23:00:22 +0200

    initial commit for fitnesse + Junit

---
+++ junit-fitnesse/README.md
@@ -0,0 +1,5 @@
+# Fitnesse Maven Junit
+
+A complete example of fitness test run 
+1. Local PC 
+2. CI servers/test indentures. 
--- junit-fitnesse/src/test/java/org/automation/CalculatorTest.java
+++ junit-fitnesse/src/test/java/org/automation/CalculatorTest.java
@@ -1,22 +1,20 @@
 package org.automation;
 
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CalculatorTest {
 
     private Calculator calculator;
     private double result;
-
-
-
-    //@Before
+    @Before
     public void init(){
         calculator = new Calculator();
     }
 
-
-   // @Test
-    //@Issue("#157")
-   // @Issues({"#156","#155"})
+    @Test
     public void testAdd() throws Throwable {
 
         Assert.assertEquals(5.0,calculator.add(3.0, 2.0),0.01);
