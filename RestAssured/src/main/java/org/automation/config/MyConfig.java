0dc9c20a3e6ada6f7390a721b272531a6f6f02d1
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-23 17:44:50 -0500

    config class , TODO

---
+++ RestAssured/src/main/java/org/automation/config/MyConfig.java
@@ -0,0 +1,74 @@
+package org.automation.config;
+
+import io.restassured.config.*;
+import io.restassured.specification.ProxySpecification;
+
+/**
+ * Created by shantonu on 12/23/16.
+ * todo, make all application specific configuration
+ */
+public class MyConfig {
+    public static ConnectionConfig getConnectionConfig(){
+        return null;
+    }
+
+    public static DecoderConfig getDecoderConfig(){
+        return null;
+    }
+
+    public static EncoderConfig getEncoderConfig(){
+        return null;
+    }
+    public static HeaderConfig getHeaderConfig(){
+        return null;
+    }
+
+    public static HttpClientConfig getHttpClientConfig(){
+        return null;
+    }
+    public static JsonConfig getJsonConfig(){
+        return null;
+    }
+
+    public static LogConfig getLogConfig(){
+        return null;
+    }
+
+    public static MatcherConfig getMatcherConfig(){
+        return null;
+    }
+    public static MultiPartConfig getMultiPartConfig(){
+        return null;
+    }
+
+    public static OAuthConfig getOAuthConfig(){
+        return null;
+    }
+    public static ObjectMapperConfig getObjectMapperConfig(){
+        return null;
+    }
+
+    public static ParamConfig getParamConfig(){
+        return null;
+    }
+
+    public static RedirectConfig getRedirectConfig(){
+        return null;
+    }
+    public static SessionConfig getSessionConfig(){
+        return null;
+    }
+
+    public static SSLConfig getSSLConfig(){
+        return null;
+    }
+    public static XmlConfig getXmlConfig(){
+        return null;
+    }
+
+    public static RestAssuredConfig getRestAssuredConfig(){
+        return null;
+    }
+
+
+}
--- RestAssured/src/main/java/org/automation/webservice/ConfigHeadersUtils.java
+++
@@ -1,7 +0,0 @@
-package org.automation.webservice;
-
-/**
- * Created by shantonu on 9/8/16.
- */
-public class ConfigHeadersUtils {
-}
