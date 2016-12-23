9957f4c99836c249fb96ca6960df5981c833166f
sarkershantonu <shantonu_oxford@yahoo.com>
2016-12-23 18:16:49 -0500

    suit class added

---
+++ RestAssured/src/test/java/org/automation/suits/RegressionTest.java
@@ -0,0 +1,13 @@
+package org.automation.suits;
+
+import org.automation.BasicTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by shantonu on 12/23/16.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BasicTests.class})
+public class RegressionTest {
+}
---
+++ RestAssured/src/test/java/org/automation/tests/ExceptionTests.java
@@ -0,0 +1,8 @@
+package org.automation.tests;
+
+/**
+ * Created by shantonu on 12/23/16.
+ * todo, all exception test cases
+ */
+public class ExceptionTests {
+}
