399ebd5c9810fb7d228ac26f3b90e233b0feec99
sarkershantonu <shantonu_oxford@yahoo.com>
2017-01-09 23:02:31 -0500

    deleted

--- RestAssured/src/main/java/org/automation/Util/CookieUtil.java
+++
@@ -1,9 +0,0 @@
-package org.automation.Util;
-
-/**
- * Created by shantonu on 12/23/16.
- * //adding custom cookie
- * todo
- */
-public class CookieUtil {
-}
--- RestAssured/src/main/java/org/automation/Util/SessionUtil.java
+++
@@ -1,8 +0,0 @@
-package org.automation.Util;
-
-/**
- * Created by shantonu on 12/23/16.
- * todo
- */
-public class SessionUtil {
-}
