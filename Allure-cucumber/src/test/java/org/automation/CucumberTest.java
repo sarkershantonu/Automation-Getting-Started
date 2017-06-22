6d18010be3073610da6808be33f0e8944b19687a
sarkershantonu <shantonu_oxford@yahoo.com>
2017-06-21 23:48:56 -0400

    only calculator

--- Allure-cucumber/src/test/java/org/automation/CucumberTest.java
+++ Allure-cucumber/src/test/java/org/automation/CucumberTest.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
  * Created by shantonu on 5/9/16.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumberReport","json:target/cucumber.json" }
-        , features = {"src/test/resources/org/automation"}
+@CucumberOptions(plugin = {"pretty", "html:target/Reports/calculator","json:target/Reports/calculator.json" }
+        , features = {"src/test/resources/org/automation/calculator.feature"}
         , glue = {""})
 public class CucumberTest {
 }
