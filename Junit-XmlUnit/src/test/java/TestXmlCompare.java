8aa55f7cad613dd020639f2974b0d99a08912741
sarkershantonu <shantonu_oxford@yahoo.com>
2017-06-30 18:26:39 -0400

    xml compare tese class

---
+++ Junit-XmlUnit/src/test/java/TestXmlCompare.java
@@ -0,0 +1,14 @@
+import org.automation.xml.XMLCompare;
+import org.junit.Test;
+
+/**
+ * Created by shantonu on 6/30/17.
+ */
+public class TestXmlCompare {
+
+
+    @Test
+    public void testXml(){
+        XMLCompare.compare("");
+    }
+}
