2e6e035908ac5e36cc017bd7730507e28e6a7b26
sarkershantonu <shantonu_oxford@yahoo.com>
2017-07-06 03:41:10 -0400

    winium desktop app with service mode

--- WiniumJunit/src/main/java/org/automation/win/core/WindowsDesktop.java
+++ WiniumJunit/src/main/java/org/automation/win/core/WindowsDesktop.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.winium.DesktopOptions;
 import org.openqa.selenium.winium.WiniumDriver;
+import org.openqa.selenium.winium.WiniumDriverService;
 import org.openqa.selenium.winium.WiniumOptions;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,6 +18,7 @@ import java.net.URL;
 public class WindowsDesktop {
     private WiniumDriver driver ;
     private WiniumOptions options;
+    private WiniumDriverService service;
     public URL host;
 
     public WindowsDesktop(){
@@ -24,6 +27,15 @@ public class WindowsDesktop {
     public WiniumDriver initDriver() throws MalformedURLException {
         host = new URL("http://"+System.getProperty("winium.host")+":"+System.getProperty("winium.port"));
         driver = new WiniumDriver(host,options);
+        return driver;
+    }
+    public WiniumDriver initDriverWithService() throws MalformedURLException {
+        service = new WiniumDriverService.Builder().
+                usingDriverExecutable(new File(System.getProperty("winium.driver.path"))).
+                usingPort(Integer.valueOf(System.getProperty("winium.port"))).
+                withVerbose(true).
+                withSilent(false).buildDesktopService();
+        driver = new WiniumDriver(service,options);
         return driver;
     }
 }
