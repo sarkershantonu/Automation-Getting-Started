0307dc574fd356727ae0f1d7f405598fc081b864
sarkershantonu <shantonu_oxford@yahoo.com>
2017-07-06 03:52:59 -0400

    soap UI test requests

---
+++ Soap-Ui-Selenium/src/main/java/org/automation/SoapUiHelper.java
@@ -0,0 +1,15 @@
+package org.automation;
+
+import com.eviware.soapui.tools.SoapUITestCaseRunner;
+
+/**
+ * Created by shantonu on 7/6/17.
+ * todo, complete a test example
+ */
+public class SoapUiHelper {
+
+    private void runWithSoapUI(String soap_ui_tc_path){
+        SoapUITestCaseRunner runner = new SoapUITestCaseRunner("./testcases/test1.xml");
+
+    }
+}
