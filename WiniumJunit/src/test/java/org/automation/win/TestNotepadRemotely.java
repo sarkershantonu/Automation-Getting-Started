95c8b129d416a3ca0b3e28db1a27353581a7c8d1
sarkershantonu <shantonu_oxford@yahoo.com>
2017-07-08 03:54:43 -0400

    seperate remote test class

---
+++ WiniumJunit/src/test/java/org/automation/win/TestNotepadRemotely.java
@@ -0,0 +1,38 @@
+package org.automation.win;
+
+import org.automation.win.app.config.PropertyUtil;
+import org.automation.win.app.notepad.Notepad;
+import org.automation.win.core.WindowsDesktop;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.winium.WiniumDriver;
+
+import java.net.MalformedURLException;
+
+/**
+ * Created by shantonu on 7/8/17.
+ */
+public class TestNotepadRemotely {
+    private static WiniumDriver driver;
+    private static Notepad notepad;
+
+
+    @BeforeClass
+    public static void init() throws MalformedURLException {
+        PropertyUtil.initProperties();
+        driver = new WindowsDesktop().initDriverInRemoteVM();
+        notepad = new Notepad(driver);
+
+    }
+
+    @Test
+    public void testTypeAndSaveRemotePC(){
+        notepad.type("Shantonu Demo");
+        notepad.saveFileInSpecificLocation(System.getProperty("vm.folder")+System.currentTimeMillis()+"_MyFile.txt");
+    }
+    @AfterClass
+    public static void quit(){
+        notepad.close();
+    }
+}
