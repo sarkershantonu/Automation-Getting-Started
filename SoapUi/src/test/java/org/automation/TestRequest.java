debdc46965b52e3bca1bdeddd916b9b09dd2275a
sarkershantonu <shantonu_oxford@yahoo.com>
2017-07-12 11:59:29 -0400

    blank test case

---
+++ SoapUi/src/test/java/org/automation/TestRequest.java
@@ -0,0 +1,13 @@
+package org.automation;
+
+import com.eviware.soapui.tools.SoapUITestCaseRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by shantonu on 7/6/17.
+ */
+
+
+public class TestRequest {
+
+}
