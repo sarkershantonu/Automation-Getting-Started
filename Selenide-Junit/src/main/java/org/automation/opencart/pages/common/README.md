54500a09bf2c7239704b82dfbeb5264e3b87f583
sarkershantonu <shantonu_oxford@yahoo.com>
2017-08-27 06:03:23 -0400

    a test class added

--- Selenide-Junit/src/main/java/org/automation/opencart/pages/common/README.md
+++
@@ -1,6 +0,0 @@
-# Common Package 
-This only contains page items which are coomon application wide. 
-usually a container of web elements common to all pages are candidate for this package. 
-this will follow page objects , so will have a initiate construct. 
-
-Note : Usually the non changing items will be considered. So, single time population is enough. 
---
+++ Selenide-Junit/src/test/java/org/automation/opencart/CheckAllFooterLinks.java
@@ -0,0 +1,29 @@
+package org.automation.opencart;
+
+import org.automation.TestBase;
+import org.automation.opencart.pages.common.FooterArea;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+/**
+ * Created by shantonu on 8/27/17.
+ */
+public class CheckAllFooterLinks extends TestBase {
+
+    private FooterArea footers;
+
+    @Before
+    public void init(){
+        footers = new FooterArea();
+    }
+
+    @Test
+    public void TestAllFooterLinks() {
+        open(footers.base_url);
+        $(By.cssSelector(footers.aboutUsLink_css)).click();
+        Assert.assertEquals("About us",title());
+    }
+}
