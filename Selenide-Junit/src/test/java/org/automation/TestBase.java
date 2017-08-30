22642cb58fa51143be6793dde0936d192d32a388
sarkershantonu <shantonu_oxford@yahoo.com>
2017-08-29 23:49:33 -0400

    LINK IS NOT WORKING

--- Selenide-Junit/src/test/java/org/automation/TestBase.java
+++ Selenide-Junit/src/test/java/org/automation/TestBase.java
@@ -20,11 +20,20 @@ public abstract class TestBase {
     }
 
     public static void loadProperties(){
-        Properties properties = System.getProperties();
+
         try {
-            properties.load(new FileInputStream(new File("./src/test/resources//test.properties")));
+            loadProperties("./src/test/resources/test.properties");
+            loadProperties("./src/test/resources/selenide.properties");
+            loadProperties("./src/test/resources/"+System.getProperty("test.env"));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    public static void loadProperties(String path) throws IOException {
+        Properties p = new Properties();
+        p.load(new FileInputStream(new File(path)));
+        for(String k:p.stringPropertyNames()){
+            System.setProperty(k,p.getProperty(k));
         }
     }
 
