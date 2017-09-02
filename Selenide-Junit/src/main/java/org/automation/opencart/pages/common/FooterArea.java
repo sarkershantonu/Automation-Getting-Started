f5832352e3db5d41210421d2ffb8bc60b01486be
sarkershantonu <shantonu_oxford@yahoo.com>
2017-09-01 23:49:09 -0400

    all css complete of links

--- Selenide-Junit/src/main/java/org/automation/opencart/pages/common/FooterArea.java
+++ Selenide-Junit/src/main/java/org/automation/opencart/pages/common/FooterArea.java
@@ -13,23 +13,13 @@ public class FooterArea extends PageBase {
     public String termsAndConditionLink_css =getLinkCSS(base_url +"/index.php?route=information/information&information_id=5") ;
     public String contactUsLink_css = getLinkCSS(base_url + "/index.php?route=information/contact");
     public String returnsLink_css=getLinkCSS(base_url + "/index.php?route=account/return/add");
-
     public String siteMapLink_css=getLinkCSS(base_url +"/index.php?route=information/sitemap");
-    
     public String brandsLink_css=getLinkCSS(base_url +"/index.php?route=product/manufacturer");
-    @FindBy(css = "/index.php?route=account/voucher")
-    
-    public String giftVoucersLink_css = getLinkCSS(base_url +"");
-
-    @FindBy(css = "/index.php?route=affiliate/account")
-    
-    public String affiliatesLink_css=getLinkCSS(base_url +"");
-    @FindBy(css = "/index.php?route=product/special")
-    
-    public String specialsLink_css = getLinkCSS(base_url +"");
+    public String giftVoucersLink_css = getLinkCSS(base_url +"/index.php?route=account/voucher");
+    public String affiliatesLink_css=getLinkCSS(base_url +"/index.php?route=affiliate/account");
+    public String specialsLink_css = getLinkCSS(base_url +"/index.php?route=product/specia");
 
     @FindBy(css = "a[title='My Account")
-    
     public String myAccountLink_css=getLinkCSS(base_url +"");
     @FindBy(css = "#column-right div.list-group a:nth-of-type(7)")
     
