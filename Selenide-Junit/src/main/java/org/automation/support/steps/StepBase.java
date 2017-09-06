ba61fee7ae3eb9059a55e89fccb29913af7fc69f
sarkershantonu <shantonu_oxford@yahoo.com>
2017-09-05 23:58:08 -0400

    steps declearation

---
+++ Selenide-Junit/src/main/java/org/automation/support/steps/StepBase.java
@@ -0,0 +1,7 @@
+package org.automation.support.steps;
+
+/**
+ * Created by shantonu on 9/5/17.
+ */
+public abstract class StepBase {
+}
---
+++ Selenide-Junit/src/main/java/org/automation/support/steps/UseSteps.java
@@ -0,0 +1,8 @@
+package org.automation.support.steps;
+
+/**
+ * Created by shantonu on 9/5/17.
+ */
+public @interface UseSteps {
+    Class<? extends StepBase> classes();
+}
