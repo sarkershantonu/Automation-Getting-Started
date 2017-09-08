f118af2ae4dcc60468b6d4a9a7c983545bdb57cb
sarkershantonu <shantonu_oxford@yahoo.com>
2017-09-07 23:59:18 -0400

    step watcher, another way to watch steps

---
+++ Selenide-Junit/src/main/java/org/automation/support/steps/junit/StepWatcher.java
@@ -0,0 +1,10 @@
+package org.automation.support.steps.junit;
+
+import org.junit.rules.TestWatcher;
+
+/**
+ * Created by shantonu on 9/7/17.
+ */
+public class StepWatcher extends TestWatcher {
+
+}
