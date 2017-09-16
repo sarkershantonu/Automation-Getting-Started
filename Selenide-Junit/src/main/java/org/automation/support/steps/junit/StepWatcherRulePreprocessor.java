e4bf5a00672eb4e49b15615d9553611e29baabc2
sarkershantonu <shantonu_oxford@yahoo.com>
2017-09-15 23:52:36 -0400

    collect all method names with @steps

--- Selenide-Junit/src/main/java/org/automation/support/steps/junit/StepWatcherRulePreprocessor.java
+++ Selenide-Junit/src/main/java/org/automation/support/steps/junit/StepWatcherRulePreprocessor.java
@@ -4,4 +4,8 @@ package org.automation.support.steps.junit;
  * Created by shantonu on 9/8/17.
  *///this will pre process rules
 public class StepWatcherRulePreprocessor {
+
+    public static void collectSteps(){
+
+    }
 }
