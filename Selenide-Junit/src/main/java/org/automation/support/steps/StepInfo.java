66739c7d3dbeca02289f81dfd596c4bca6bad9cc
sarkershantonu <shantonu_oxford@yahoo.com>
2017-09-16 03:45:03 -0400

    saving info

--- Selenide-Junit/src/main/java/org/automation/support/steps/StepInfo.java
+++ Selenide-Junit/src/main/java/org/automation/support/steps/StepInfo.java
@@ -1,9 +1,11 @@
 package org.automation.support.steps;
 
+import java.util.List;
+
 /**
  * Created by shantonu on 9/15/17.
  */
 public class StepInfo {
     public String method_name;
-    public List parameter_type;
+    public List<Object[]> parameter_type_values;
 }
