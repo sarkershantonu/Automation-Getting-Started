25f7c5a24e0a29f9b6e613270d0b313145703752
sarkershantonu <shantonu_oxford@yahoo.com>
2017-09-17 23:55:59 -0400

    parameter info

--- Selenide-Junit/src/main/java/org/automation/support/steps/ParamInfo.java
+++ Selenide-Junit/src/main/java/org/automation/support/steps/ParamInfo.java
@@ -4,27 +4,14 @@ package org.automation.support.steps;
  * Created by shantonu on 9/17/17.
  */
 public class ParamInfo {
-    private Object type;
+    private Class type;
     private Object value;
 
-    public ParamInfo(Object type, Object value) {
-        this.type = type;
-        this.value = value;
-    }
-
-    public Object getType() {
-        return type;
-    }
-
-    public void setType(Object type) {
-        this.type = type;
-    }
-
-    public Object getValue() {
-        return value;
-    }
-
-    public void setValue(Object value) {
-        this.value = value;
+    @Override
+    public String toString() {
+        return "ParamInfo{" +
+                "type=" + type.getName() +
+                ", value=" + value.toString() +
+                '}';
     }
 }
