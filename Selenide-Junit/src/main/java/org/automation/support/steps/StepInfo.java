514ab01cc5ec321aa52d196e459a6122eee05001
sarkershantonu <shantonu_oxford@yahoo.com>
2017-09-17 23:53:37 -0400

    info for all parameters

--- Selenide-Junit/src/main/java/org/automation/support/steps/StepInfo.java
+++ Selenide-Junit/src/main/java/org/automation/support/steps/StepInfo.java
@@ -1,6 +1,7 @@
 package org.automation.support.steps;
 
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * Created by shantonu on 9/15/17.
@@ -8,4 +9,15 @@ import java.util.List;
 public class StepInfo {
     public String method_name;
     public List<ParamInfo> parameters;
+
+    @Override
+    public String toString() {
+        String info = "method_name= " + method_name +",\nparameters= ";
+
+        for(ParamInfo item : parameters){
+            info+=item.toString()+",";
+        }
+
+        return info;
+    }
 }
