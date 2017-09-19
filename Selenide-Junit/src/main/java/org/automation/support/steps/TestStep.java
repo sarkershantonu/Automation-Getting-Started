177d5a2b42501c8aec14f48719b1e9ed5dfb3b90
sarkershantonu <shantonu_oxford@yahoo.com>
2017-09-18 23:51:15 -0400

    test step

--- Selenide-Junit/src/main/java/org/automation/support/steps/TestStep.java
+++ Selenide-Junit/src/main/java/org/automation/support/steps/TestStep.java
@@ -11,4 +11,6 @@ import java.lang.annotation.Target;
 @Retention(value = RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface TestStep {
+    //thsi is declearing test step
+
 }
