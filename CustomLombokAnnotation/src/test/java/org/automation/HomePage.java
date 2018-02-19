4608c685265e171e878ae3c476532e720dc780ec
Shantonu Sarker <shantonu_oxford@yahoo.com>
2018-02-18 22:45:04 -0500

    updated

--- CustomLombokAnnotation/src/test/java/org/automation/HomePage.java
+++ CustomLombokAnnotation/src/test/java/org/automation/HomePage.java
@@ -1,7 +1,16 @@
 package org.automation;
 
 
+import org.automation.annotations.Click;
+import org.automation.annotations.Type;
+import org.openqa.selenium.WebElement;
+
 public class HomePage {
 
 
+    @Click//this will create a method click_search_btn() which will invoke clicking
+    public WebElement search_btn;
+
+    @Type//this will create a method type_search_txt(CharacterSequence characters)
+    public WebElement search_txt;
 }
