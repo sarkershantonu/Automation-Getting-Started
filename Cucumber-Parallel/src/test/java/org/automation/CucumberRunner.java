9c8f8a5e0f012d1a999ae7b74fb8e9bd15d7c312
shantonu sarker <shantonu_oxford@yahoo.com>
2018-07-08 20:45:53 -0400

    old cucumber runner

---
+++ Cucumber-Parallel/src/test/java/org/automation/CucumberRunner.java
@@ -0,0 +1,15 @@
+package org.automation;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by shantonu on 5/9/16.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumberReport","json:target/cucumberReport/cucumber.json" }
+        , features = {"src/test/resources/org/automation/calculator-add.feature"}
+        , glue = {""})
+public class CucumberRunner {
+}
