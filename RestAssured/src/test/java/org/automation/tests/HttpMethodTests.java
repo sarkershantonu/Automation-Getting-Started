d55793017f94c791e8c521b9cf989e2cf7d542af
shantonu sarker <shantonu_oxford@yahoo.com>
2018-08-05 02:11:35 -0400

    basic method tests

---
+++ RestAssured/src/test/java/org/automation/tests/HttpMethodTests.java
@@ -0,0 +1,10 @@
+package org.automation.tests;
+
+import org.automation.core.BugTestBase;
+
+/**
+ * Created by SSarker on 8/5/2018.
+ */
+//this will test only allowed method are accessible.
+public class HttpMethodTests extends BugTestBase {
+}
