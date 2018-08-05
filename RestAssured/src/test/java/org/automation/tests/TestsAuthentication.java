cbbc8da4a332c5b22cc90f5b1959c46dedfe48d7
shantonu sarker <shantonu_oxford@yahoo.com>
2018-08-05 00:55:40 -0400

    tests

--- RestAssured/src/test/java/org/automation/tests/TestsAuthentication.java
+++ RestAssured/src/test/java/org/automation/tests/TestsAuthentication.java
@@ -1,12 +1,18 @@
 package org.automation.tests;
 
+import org.apache.http.HttpStatus;
 import org.automation.core.BugTestBase;
 import org.automation.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Set;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThan;
 
 /**
  * Created by SSarker on 8/4/2018.
@@ -16,8 +22,20 @@ public class TestsAuthentication extends BugTestBase {
     @Parameterized.Parameter
     public User credential;
 
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<User> credentials() {
+        return Arrays.asList(new User[]{
+                new User("", ""),
+                new User("shantonu", "~!@#$%^")
+        });
+    }
 
 
     @Test
-    public void
+    public void testBadAuthentications() {
+       given()
+               .auth().basic(credential.getName(),credential.getPassword())
+               .get().then().assertThat().statusCode(HttpStatus.SC_UNAUTHORIZED)
+               .time(lessThan(globalResponseTimeout));
+    }
 }
