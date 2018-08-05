9fda2aa8420a50b7603bf727cfe3ce0aceed8c68
shantonu sarker <shantonu_oxford@yahoo.com>
2018-08-05 00:55:34 -0400

    user

--- RestAssured/src/main/java/org/automation/model/User.java
+++ RestAssured/src/main/java/org/automation/model/User.java
@@ -30,4 +30,12 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
