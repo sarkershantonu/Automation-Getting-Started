238e906d7e77cc1526ff4d8d0cecb2b50b8a220a
shantonu sarker <shantonu_oxford@yahoo.com>
2018-08-04 23:55:50 -0400

    credential object

---
+++ RestAssured/src/main/java/org/automation/model/User.java
@@ -0,0 +1,33 @@
+package org.automation.model;
+
+/**
+ * Created by SSarker on 8/4/2018.
+ */
+public class User {
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
