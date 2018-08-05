76c9e0ef7bed107d30fc3a4a0154a602f92b41e3
shantonu sarker <shantonu_oxford@yahoo.com>
2018-08-04 23:56:15 -0400

    test for auth test

---
+++ RestAssured/src/test/java/org/automation/tests/TestsAuthentication.java
@@ -0,0 +1,23 @@
+package org.automation.tests;
+
+import org.automation.core.BugTestBase;
+import org.automation.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Set;
+
+/**
+ * Created by SSarker on 8/4/2018.
+ */
+@RunWith(Parameterized.class)
+public class TestsAuthentication extends BugTestBase {
+    @Parameterized.Parameter
+    public User credential;
+
+
+
+    @Test
+    public void
+}
