e97095d54ba7b199c8fb3e85f816ee1a0cd413fc
shantonu sarker <shantonu_oxford@yahoo.com>
2018-08-04 23:45:00 -0400

    type mistake

--- RestAssured/src/test/java/org/automation/core/BugTestBase.java
+++ RestAssured/src/test/java/org/automation/core/BugTestBase.java
@@ -2,13 +2,14 @@ package org.automation.core;
 
 import io.restassured.RestAssured;
 import io.restassured.mapper.ObjectMapperType;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 /**
  * Created by shantonu on 12/23/16.
  */
 // following best practice, I am putting my all settings in before class so that it can be initialized before test class.// following best practice, I am putting my all settings in before class so that it can be initialized before test class.
-public abstract class BugTestBase {
+public abstract class BugTestBase extends TestBase{
     protected static final String URL = "http://localhost:9100";
     protected static final String user = "shantonu";
     protected static final String pass = "123456";
@@ -20,7 +21,7 @@ public abstract class BugTestBase {
         RestAssured.baseURI = URL;
         RestAssured.basePath = "/table/bugs/";
     }
-    @BeforeClass
+    @AfterClass
     public static void cleanupClass(){
         // causing errors => RestAssured.reset();
     }
