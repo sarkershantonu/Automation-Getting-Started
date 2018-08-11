85cff8ecc01c0336127b8141e240fe3de96c6450
shantonu sarker <shantonu_oxford@yahoo.com>
2018-08-11 02:15:32 -0400

    basic

--- RestAssured/src/test/java/org/automation/tests/HttpMethodTests.java
+++ RestAssured/src/test/java/org/automation/tests/HttpMethodTests.java
@@ -11,19 +11,20 @@ import static io.restassured.RestAssured.given;
  * Created by SSarker on 8/5/2018.
  */
 //this will test only allowed method are accessible.
+    //https://www.owasp.org/index.php/REST_Security_Cheat_Sheet
 public class HttpMethodTests extends BugTestBase {
 
     @Test
     public void test_options_method_off(){
-        given().auth().basic(user,pass+5).options().then().assertThat().statusCode(HttpStatus.SC_UNAUTHORIZED);
+        given().auth().basic(user,pass+5).options().then().assertThat().statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
     }
     @Test
     public void test_head_method_off(){
-        given().auth().basic(user,pass+5).head().then().assertThat().statusCode(HttpStatus.SC_UNAUTHORIZED);
+        given().auth().basic(user,pass+5).head().then().assertThat().statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
     }
     @Test
     public void test_patch_method_off(){
-        given().auth().basic(user,pass+5).patch().then().assertThat().statusCode(HttpStatus.SC_UNAUTHORIZED);
+        given().auth().basic(user,pass+5).patch().then().assertThat().statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
     }
 
 }
