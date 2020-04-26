f6b2965e1ae693c7fb0c935c35c3d1a24049de1a
shantonu sarker <sarker.shantonu@gmail.com>
2020-04-26 23:01:04 +0600

    calc

--- Allure2-Junit5/src/main/java/org/automation/Calculator.java
+++ Allure2-Junit5/src/main/java/org/automation/Calculator.java
@@ -1,4 +1,31 @@
 package org.automation;
 
 public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int[] a) {
+        int result = 0;
+        for (int i : a) {
+            result += i;
+        }
+        return result;
+    }
+
+    public Integer add(Integer... numbers) {
+        Integer result = new Integer(0);
+        for (Integer i : numbers) {
+            result += i;
+        }
+        return result;
+    }
+
+    public <T extends Number> T add(T... t) {
+        Double sum = new Double(0.0);
+        for (T i : t) {
+            sum += i.doubleValue();
+        }
+        return (T) sum;
+    }
 }
