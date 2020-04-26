fd455a21e774cab391aab25f3e6fc27673217b5c
shantonu sarker <sarker.shantonu@gmail.com>
2020-04-26 23:34:45 +0600

    base test

--- Allure2-Junit5/src/test/java/org/automation/junit5/CalculatorTestBase.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/CalculatorTestBase.java
@@ -1,4 +1,19 @@
 package org.automation.junit5;
 
-public class TestBase {
+import org.automation.Calculator;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class CalculatorTestBase {
+    protected Calculator myCal;
+
+    @BeforeAll
+    public static void initForAllTest(){
+
+    }
+    @AfterAll
+    public static void cleanupForAllTest(){
+
+    }
+
 }
