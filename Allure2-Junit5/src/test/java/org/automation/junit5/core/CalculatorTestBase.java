f4ee651d3066106220c4db9fd5e8a610b97633eb
shantonu sarker <sarker.shantonu@gmail.com>
2020-04-27 17:54:05 +0600

    step logging implemented

--- Allure2-Junit5/src/test/java/org/automation/junit5/core/CalculatorTestBase.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/core/CalculatorTestBase.java
@@ -1,11 +1,16 @@
 package org.automation.junit5.core;
 
+import io.qameta.allure.listener.StepLifecycleListener;
+import io.qameta.allure.model.StepResult;
 import org.automation.Calculator;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class CalculatorTestBase {
+public class CalculatorTestBase implements StepLifecycleListener {
     protected Calculator myCal;
+    public Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @BeforeAll
     public static void initForAllTest(){
@@ -16,4 +21,8 @@ public class CalculatorTestBase {
 
     }
 
+    @Override
+    public void beforeStepStop(StepResult result) {
+        logger.info("Finished step : {}",result.getName());
+    }
 }
