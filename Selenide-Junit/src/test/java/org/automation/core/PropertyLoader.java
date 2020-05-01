c6d89a47c9e7ebcf59fb78b5d53f8e619ebfb85b
shantonu sarker <sarker.shantonu@gmail.com>
2020-05-01 09:24:40 +0600

    property loader added

--- Selenide-Junit/src/test/java/org/automation/core/PropertyLoader.java
+++ Selenide-Junit/src/test/java/org/automation/core/PropertyLoader.java
@@ -1,4 +1,30 @@
 package org.automation.core;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyLoader {
+    public static final String PROP_PATH="./properties/";
+    public static void loadProperties(){
+
+        try {
+            loadProperties(PROP_PATH+"test.properties");
+            loadProperties(PROP_PATH+ "properties/selenide.properties");
+            loadProperties(PROP_PATH
+                            +System.getProperty("test.app.name")
+                            +System.lineSeparator()
+                            +System.getProperty("test.env")+".properties");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void loadProperties(String path) throws IOException {
+        Properties p = new Properties();
+        p.load(new FileInputStream(new File(path)));
+        for(String k: p.stringPropertyNames()){
+            System.setProperty(k,p.getProperty(k));
+        }
+    }
 }
