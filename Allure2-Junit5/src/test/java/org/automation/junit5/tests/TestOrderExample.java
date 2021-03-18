3e34b1dd944df0e637b7ef38282314551838bf7a
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-19 00:04:14 +0600

    test order example

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/TestOrderExample.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/TestOrderExample.java
@@ -1,4 +1,9 @@
 package org.automation.junit5.tests;
 
-public class TestOrderExample {
+import org.automation.junit5.core.CalculatorTestBase;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestOrderExample extends CalculatorTestBase {
 }
