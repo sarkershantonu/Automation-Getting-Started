894fb6d854783cf2f9f72e3fe4f59191984e4c2c
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-19 21:53:45 +0600

    parameter testing

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/ParameterizedTestExample.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/ParameterizedTestExample.java
@@ -1,8 +1,17 @@
 package org.automation.junit5.tests;
 
 import org.automation.junit5.core.CalculatorTestBase;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ParameterizedTestExample extends CalculatorTestBase {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/addition.csv",numLinesToSkip = 1)
+    public void testCSVfileParameter(){
+
+    }
 
 
 }
