f197e325666519249827ac40e48f461c202a0522
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-19 23:42:31 +0600

    parameter testing value source

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/ParameterizedTestExample.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/ParameterizedTestExample.java
@@ -1,6 +1,7 @@
 package org.automation.junit5.tests;
 
 import org.automation.junit5.core.CalculatorTestBase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -11,7 +12,7 @@ public class ParameterizedTestExample extends CalculatorTestBase {
     @ParameterizedTest
     @CsvFileSource(resources = "/addition.csv",numLinesToSkip = 1)
     public void testCSVfileParameter(String a, String b, String result){
-
+        Assertions.assertEquals(Double.valueOf(result).doubleValue(),myCal.add(Double.valueOf(a).doubleValue(),Double.valueOf(b).doubleValue()));
     }
 
     @ParameterizedTest
@@ -21,9 +22,9 @@ public class ParameterizedTestExample extends CalculatorTestBase {
     }
 
     @ParameterizedTest
-    @ValueSource
-    public void testValueSource(){
-
+    @ValueSource(ints = {5,6,7})
+    public void testValueSource(int input){
+        Assertions.assertEquals(input*2,myCal.add(input,input));
     }
 
 }
