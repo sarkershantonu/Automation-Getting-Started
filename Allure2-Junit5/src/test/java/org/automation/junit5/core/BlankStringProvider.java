040986b5160dc660c9eeda7cc6b8bf1b3d2c9b0d
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-21 05:59:21 +0600

    argument provider

---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/core/BlankStringProvider.java
@@ -0,0 +1,15 @@
+package org.automation.junit5.core;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import java.util.stream.Stream;
+
+public class BlankStringProvider implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+        return Stream.of(Arguments.of((String)null),Arguments.of(""),Arguments.of(" "));
+    }
+
+}
