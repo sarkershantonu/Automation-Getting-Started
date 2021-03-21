ba4cd35c47da9bc154e1c387a2d5b47dd68c1d29
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-21 21:05:14 +0600

    VariableArgumentProvider

---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/core/VariableStream.java
@@ -0,0 +1,15 @@
+package org.automation.junit5.core;
+
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@ArgumentsSource(VariableArgumentProvider.class)
+public @interface VariableStream {
+    String value();
+}
