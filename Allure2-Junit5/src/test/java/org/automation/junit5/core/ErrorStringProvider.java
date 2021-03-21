ff2f372d79e7fd8efb0844f9d0fffebaaba8314f
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-21 20:38:55 +0600

    argument provider

--- Allure2-Junit5/src/test/java/org/automation/junit5/core/ErrorStringProvider.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/core/ErrorStringProvider.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.ArgumentsProvider;
 
 import java.util.stream.Stream;
 
-public class BlankStringProvider implements ArgumentsProvider {
+public class ErrorStringProvider implements ArgumentsProvider {
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
         return Stream.of(Arguments.of((String)null),Arguments.of(""),Arguments.of(" "));
