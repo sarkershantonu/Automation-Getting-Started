22378e44b10fb9c75dd7e0bd8902aef80509567a
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-23 05:56:22 +0600

    custom annotation tests

---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/core/CalculatorTest.java
@@ -0,0 +1,24 @@
+package org.automation.junit5.core;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Link;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Test
+@Timeout(2)
+//@DisplayName("Calculator Test With Custom Annotation")
+@Epic("Epic Calculator")
+@Link("aLink")
+public @interface CalculatorTest {
+}
---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/TestCustomAnnotation.java
@@ -0,0 +1,20 @@
+package org.automation.junit5.tests;
+
+import org.automation.Calculator;
+import org.automation.junit5.core.CalculatorTest;
+import org.automation.junit5.core.CalculatorTestBase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCustomAnnotation extends CalculatorTestBase {
+    @BeforeEach
+    public void initTest(){
+        myCal = new Calculator();
+    }
+    @CalculatorTest
+    public void testWithCustomAnnotation(){
+        assertEquals(30,myCal.add(25,5));
+    }
+}
