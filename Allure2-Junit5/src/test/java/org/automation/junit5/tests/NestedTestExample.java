b222f74baaaad49d0157f2e0e4d4352da7414792
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-23 01:10:29 +0600

    renamed test cases

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/NestedTestExample.java
+++
@@ -1,7 +0,0 @@
-package org.automation.junit5.tests;
-
-import org.junit.jupiter.api.Nested;
-
-@Nested
-public class NestedTestExample {
-}
---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/TestNestedTestExample.java
@@ -0,0 +1,11 @@
+package org.automation.junit5.tests;
+
+import org.junit.jupiter.api.Nested;
+
+
+public class TestNestedTestExample {
+    @Nested
+    class TestNEsted{
+
+    }
+}
