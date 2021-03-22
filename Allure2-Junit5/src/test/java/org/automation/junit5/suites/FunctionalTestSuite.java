de961a8630e3794fb52714e02848b856b136e675
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-23 01:07:43 +0600

    suite experiments

--- Allure2-Junit5/src/test/java/org/automation/junit5/suites/FunctionalTestSuite.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/suites/FunctionalTestSuite.java
@@ -1,7 +1,17 @@
 package org.automation.junit5.suites;
 
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
 /**
  * Suite Example
  */
+@RunWith(JUnitPlatform.class)
+//@SelectClasses(ReleaseASuite.class)
+@SelectPackages({"org.automation.junit5.tests"})
+//@IncludeTags({"releaseB"})
 public class FunctionalTestSuite {
 }
