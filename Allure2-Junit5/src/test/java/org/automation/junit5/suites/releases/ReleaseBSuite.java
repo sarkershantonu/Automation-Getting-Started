85ea338d444cdd2ecd0c5f4ccbc188662e621148
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-23 01:07:32 +0600

    suite experiments

---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/suites/releases/ReleaseBSuite.java
@@ -0,0 +1,12 @@
+package org.automation.junit5.suites.releases;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@IncludeTags({"releaseB"})
+@SelectPackages("org.automation.junit5.tests")
+public class ReleaseBSuite {
+}
