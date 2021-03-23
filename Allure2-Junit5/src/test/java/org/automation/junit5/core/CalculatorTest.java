390724e03a3cd1d40a1d8e1f6282c8cffd8fa1bf
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-24 05:29:57 +0600

    custom annotation experiment

--- Allure2-Junit5/src/test/java/org/automation/junit5/core/CalculatorTest.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/core/CalculatorTest.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Link;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
@@ -20,5 +21,6 @@ import java.lang.annotation.Target;
 //@DisplayName("Calculator Test With Custom Annotation")
 @Epic("Epic Calculator")
 @Link("aLink")
+@Tag("CalT")
 public @interface CalculatorTest {
 }
