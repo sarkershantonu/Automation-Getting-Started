806ad6b23f54c453261f0bab1ae671bb24f63aa6
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-29 23:54:39 +0600

    demotest class for allure

---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/allure/TestExamplesForAllureReports.java
@@ -0,0 +1,9 @@
+package org.automation.junit5.allure;
+
+import org.automation.junit5.core.CalculatorTestBase;
+
+/**
+ * Created by shantonu on 3/29/2021
+ */
+public class TestExamplesForAllureReports extends CalculatorTestBase {
+}
