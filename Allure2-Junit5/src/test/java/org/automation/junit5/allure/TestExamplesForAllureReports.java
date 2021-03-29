6017f8e67669eee8e4456060864fa288402f9d58
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-29 23:59:37 +0600

    allure test calsss

--- Allure2-Junit5/src/test/java/org/automation/junit5/allure/TestExamplesForAllureReports.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/allure/TestExamplesForAllureReports.java
@@ -1,9 +1,39 @@
 package org.automation.junit5.allure;
 
 import org.automation.junit5.core.CalculatorTestBase;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by shantonu on 3/29/2021
  */
 public class TestExamplesForAllureReports extends CalculatorTestBase {
+    @Test
+    public void testAdd(){
+        assertEquals(30,myCal.add(25,5));
+    }
+    @Test
+    public void testAddIntArray(){
+        int[] data = {5,10,25,6,4};
+        assertEquals(50,myCal.add(data));
+    }
+    @Test
+    public void testAddIntegerArray(){
+        Integer[] data = new Integer[]{5,10,25,6,4};
+        assertEquals(50,myCal.add(data));
+    }
+    @Test
+    public void testAddMixType(){
+        Double[] data_double = new Double[]{15.0,25.1,4.9};
+        Integer[] data_int = new Integer[]{15,5,10};
+        assertEquals(45.0,myCal.add(data_double));
+        assertEquals(30,myCal.add(data_int));
+    }
+    @Test
+    public void testAddDecimal(){
+        double a = 2.4, b =5.2;
+        assertEquals(7.6, myCal.add(a,b), "double adding ");
+    }
 }
