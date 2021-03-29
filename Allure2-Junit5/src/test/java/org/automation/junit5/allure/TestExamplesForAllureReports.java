a9e4f43535c08b4084f3ba20d3d0c9f1496ef880
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-30 00:00:50 +0600

    @Severity

--- Allure2-Junit5/src/test/java/org/automation/junit5/allure/TestExamplesForAllureReports.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/allure/TestExamplesForAllureReports.java
@@ -1,5 +1,7 @@
 package org.automation.junit5.allure;
 
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.automation.junit5.core.CalculatorTestBase;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -9,22 +11,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Created by shantonu on 3/29/2021
  */
+
 public class TestExamplesForAllureReports extends CalculatorTestBase {
     @Test
+    @Severity(SeverityLevel.BLOCKER)
     public void testAdd(){
         assertEquals(30,myCal.add(25,5));
     }
     @Test
+    @Severity(SeverityLevel.MINOR)
     public void testAddIntArray(){
         int[] data = {5,10,25,6,4};
         assertEquals(50,myCal.add(data));
     }
     @Test
+    @Severity(SeverityLevel.CRITICAL)
     public void testAddIntegerArray(){
         Integer[] data = new Integer[]{5,10,25,6,4};
         assertEquals(50,myCal.add(data));
     }
     @Test
+    @Severity(SeverityLevel.TRIVIAL)
     public void testAddMixType(){
         Double[] data_double = new Double[]{15.0,25.1,4.9};
         Integer[] data_int = new Integer[]{15,5,10};
@@ -32,6 +39,7 @@ public class TestExamplesForAllureReports extends CalculatorTestBase {
         assertEquals(30,myCal.add(data_int));
     }
     @Test
+    @Severity(SeverityLevel.NORMAL)
     public void testAddDecimal(){
         double a = 2.4, b =5.2;
         assertEquals(7.6, myCal.add(a,b), "double adding ");
