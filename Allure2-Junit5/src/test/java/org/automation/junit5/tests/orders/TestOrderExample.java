d3337f4c0261461086a9164321e83c5f0bb80574
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-31 22:55:09 +0600

    test order by value

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/orders/TestOrderExample.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/orders/TestOrderExample.java
@@ -1,9 +1,46 @@
 package org.automation.junit5.tests.orders;
 
+import org.automation.Calculator;
 import org.automation.junit5.core.CalculatorTestBase;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestOrderExample extends CalculatorTestBase {
+    @BeforeEach
+    public void initTest(){
+        myCal = new Calculator();
+    }
+    @Test
+    @Order(1)
+    public void testAddSingle(){
+        assertEquals(30,myCal.add(25,5));
+    }
+    @Test
+    @Order(5)
+    public void testAddInt(){
+        int[] data = {5,10,25,6,4};
+        assertEquals(50,myCal.add(data));
+    }
+    @Test
+    @Order(3)
+    public void testAddArray(){
+        Integer[] data = new Integer[]{5,10,25,6,4};
+        assertEquals(50,myCal.add(data));
+    }
+    @Test
+    @Order(2)
+    public void testAddGeneric(){
+        Double[] data_double = new Double[]{15.0,25.1,4.9};
+        Integer[] data_int = new Integer[]{15,5,10};
+        assertEquals(45.0,myCal.add(data_double));
+        assertEquals(30,myCal.add(data_int));
+    }
+    @Test
+    @Order(4)
+    public void testAddDecimal(){
+        double a = 2.4, b =5.2;
+        assertEquals(7.6, myCal.add(a,b), "double adding ");
+    }
 }
