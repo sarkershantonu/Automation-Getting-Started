4770bea717607d72be6cf2883665b9c5599eb933
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-31 22:53:20 +0600

    test order complete

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/orders/TestRandomOrderExample.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/orders/TestRandomOrderExample.java
@@ -13,23 +13,24 @@ public class TestRandomOrderExample extends CalculatorTestBase {
         myCal = new Calculator();
     }
     @Test
+    @Order(1)
     public void testAddSingle(){
         assertEquals(30,myCal.add(25,5));
     }
     @Test
+    @Order(5)
     public void testAddInt(){
         int[] data = {5,10,25,6,4};
         assertEquals(50,myCal.add(data));
     }
     @Test
-    @Tag("releaseA")
-    @Tag("releaseB")
+    @Order(3)
     public void testAddArray(){
         Integer[] data = new Integer[]{5,10,25,6,4};
         assertEquals(50,myCal.add(data));
     }
     @Test
-    @Tag("releaseA")
+    @Order(2)
     public void testAddGeneric(){
         Double[] data_double = new Double[]{15.0,25.1,4.9};
         Integer[] data_int = new Integer[]{15,5,10};
@@ -37,7 +38,7 @@ public class TestRandomOrderExample extends CalculatorTestBase {
         assertEquals(30,myCal.add(data_int));
     }
     @Test
-    @Tag("releaseB")
+    @Order(4)
     public void testAddDecimal(){
         double a = 2.4, b =5.2;
         assertEquals(7.6, myCal.add(a,b), "double adding ");
