235109f98afc6361a510cd571857f7a90cc4e5b5
shantonu sarker <sarker.shantonu@gmail.com>
2021-04-02 21:49:41 +0600

    testEnumSourceCustomName

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/parameterized/TestParameterized.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/parameterized/TestParameterized.java
@@ -90,36 +90,5 @@ public class TestParameterized extends CalculatorTestBase {
     public void testEmptyNullSource(String input_Null_empty){
         assertTrue(isBlank(input_Null_empty));
     }
-    @ParameterizedTest
-    @EnumSource(Month.class)
-    public void testEnumSource(Month month){
-        assertTrue(month.getValue()>=1&& month.getValue()<=12);
-    }
-    @ParameterizedTest
-    @EnumSource
-    public void testEnumSourceNoClass(Month month){
-        assertTrue(month.getValue()>=1&& month.getValue()<=12);
-    }
 
-    /**
-     * Non Leap Year testing
-     * false = not leap year
-     * @param month
-     */
-    @ParameterizedTest
-    @EnumSource(value = Month.class, names = {"JANUARY","MARCH","MAY","JULY"})
-    public void testEnumSourceWithData(Month month){
-        assertEquals(31,month.length(false));
-    }
-    @ParameterizedTest
-    @EnumSource(value = Month.class, names = { "FEBRUARY","APRIL", "JUNE", "SEPTEMBER", "NOVEMBER"}, mode = EnumSource.Mode.EXCLUDE)
-    public void testEnumSourceExcludeData(Month month){
-        assertEquals(31,month.length(false));
-    }
-    @ParameterizedTest
-    @EnumSource(value = Month.class, names = ".+Y",mode = EnumSource.Mode.MATCH_ANY)
-    public void testEnumWithDataRegex(Month month){
-        assertTrue(month.toString().endsWith("Y"),"Not Ends with Y");
-
-    }
 }
