9a437241bafcebf2ac753e0dbad4503c912e13e4
shantonu sarker <sarker.shantonu@gmail.com>
2021-04-03 23:26:35 +0600

    testAggregatedWithCSV - user

---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/parameterized/User.java
@@ -0,0 +1,29 @@
+package org.automation.junit5.tests.parameterized;
+
+/**
+ * Created by shantonu on 4/3/2021
+ */
+public class User {
+    private final String fName;
+    private final String mName;
+    private final String lName;
+
+    public User(String fName, String mName, String lName) {
+        this.fName = fName;
+        this.mName = mName;
+        this.lName = lName;
+    }
+    public String getName(){
+        if((null==mName)||mName.trim().isEmpty())
+            {
+            return  String.format("%s %s", fName,lName);
+            }
+        else
+            {
+        return String.format("%s %s %s", fName,mName,lName);
+            }
+    }
+    public String fullName(){
+        return this.getName();
+    }
+}
