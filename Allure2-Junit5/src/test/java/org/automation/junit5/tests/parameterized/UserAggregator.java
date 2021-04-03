6daa311f59f5c28e7f9c44742aaeaf1952f05d52
shantonu sarker <sarker.shantonu@gmail.com>
2021-04-03 23:28:27 +0600

    testAggregatedWithCSV - UserAggregator

---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/parameterized/UserAggregator.java
@@ -0,0 +1,20 @@
+package org.automation.junit5.tests.parameterized;
+
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+
+/**
+ * Created by shantonu on 4/3/2021
+ */
+public class UserAggregator implements ArgumentsAggregator {
+    @Override
+    public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+
+        return new User(
+                accessor.getString(1),
+                accessor.getString(2),
+                accessor.getString(3));
+    }
+}
