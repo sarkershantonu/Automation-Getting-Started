b95b480ce502476d64bf8513de5b009c127da02a
shantonu sarker <sarker.shantonu@gmail.com>
2021-04-11 23:48:49 +0600

    groupping with tags

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/TestGroupExample.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/TestGroupExample.java
@@ -1,9 +1,32 @@
 package org.automation.junit5.tests;
 
 import org.automation.junit5.core.CalculatorTestBase;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /***
  * Tagging
  */
 public class TestGroupExample extends CalculatorTestBase {
+    public void testAddArray(){
+        Integer[] data = new Integer[]{5,10,25,6,4};
+        assertEquals(50,myCal.add(data));
+    }
+    @Test
+    @Tag("parallel")
+    @Tag("quick")
+    public void testAddGeneric(){
+        Double[] data_double = new Double[]{15.0,25.1,4.9};
+        Integer[] data_int = new Integer[]{15,5,10};
+        assertEquals(45.0,myCal.add(data_double));
+        assertEquals(30,myCal.add(data_int));
+    }
+    @Test
+    @Tag("slow")
+    public void testAddDecimal(){
+        double a = 2.4, b =5.2;
+        assertEquals(7.6, myCal.add(a,b), "double adding ");
+    }
 }
