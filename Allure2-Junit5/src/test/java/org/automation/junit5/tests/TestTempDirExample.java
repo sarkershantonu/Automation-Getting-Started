a78f3bf175d3131b4e20cfb659249811072670b5
shantonu sarker <sarker.shantonu@gmail.com>
2021-04-15 23:59:29 +0600

    todo temp directory

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/TestTempDirExample.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/TestTempDirExample.java
@@ -1,4 +1,8 @@
 package org.automation.junit5.tests;
 
+/***
+ * todo https://www.baeldung.com/junit-5-temporary-directory
+ * https://junit.org/junit5/docs/5.4.0-RC1/api/org/junit/jupiter/api/support/io/TempDirectory.html
+ */
 public class TestTempDirExample {
 }
