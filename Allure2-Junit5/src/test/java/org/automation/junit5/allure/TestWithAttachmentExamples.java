5a9969d016b0f17313e5881a880aa8b4a9541253
shantonu sarker <sarker.shantonu@gmail.com>
2021-04-24 20:03:22 +0600

    attahchment with PNG

---
+++ Allure2-Junit5/src/test/java/org/automation/junit5/allure/TestWithAttachmentExamples.java
@@ -0,0 +1,41 @@
+package org.automation.junit5.allure;
+
+import io.qameta.allure.*;
+import org.automation.junit5.core.CalculatorTestBase;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by shantonu on 4/24/2021
+ */
+public class TestWithAttachmentExamples extends CalculatorTestBase {
+
+
+    @Test
+    public void testAddWIthScreenshot() throws IOException, AWTException {
+        assertEquals(30,myCal.add(25,5));
+        capturePNG();
+    }
+
+    @Attachment
+    public byte[] capturePNG() throws IOException, AWTException {
+        byte[] out = null;
+            BufferedImage screencapture = new Robot().createScreenCapture(
+                    new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            ImageIO.write(screencapture, "png", bo);
+            out = bo.toByteArray();
+            bo.close();
+        return out;
+    }
+
+
+
+}
