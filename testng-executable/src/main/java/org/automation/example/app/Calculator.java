8dd1d7484a1279aac1553bdef5440051fbd15f84
shantonu sarker <sarker.shantonu@gmail.com>
2022-09-24 23:34:17 -0500

    tet class

---
+++ testng-executable/src/main/java/org/automation/example/app/Calculator.java
@@ -0,0 +1,8 @@
+package org.automation.example.app;
+
+public class Calculator {
+
+    public double add(double a, double b){return a+b;}
+    public int add(int a, int b){return a+b;}
+    public long add(long a, long b){return a+b;}
+}
