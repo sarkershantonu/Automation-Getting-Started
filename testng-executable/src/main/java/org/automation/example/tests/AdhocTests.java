b71b2a3cc0ab79c391a56d7a72640f94871d4b39
shantonu sarker <sarker.shantonu@gmail.com>
2022-09-26 00:13:51 -0500

    Test Case Updates

---
+++ testng-executable/src/main/java/org/automation/example/tests/AdhocTests.java
@@ -0,0 +1,18 @@
+package org.automation.example.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AdhocTests extends CalculatorBaseTest {
+
+
+    @Test
+    public void testIntAdd(){
+        Assert.assertEquals(calculator.add(5,3),8,"Addition Fail");
+    }
+
+    @Test
+    public void testFloatAdd(){
+        Assert.assertEquals(calculator.add(5.8,3.3),9.1,"Addition Fail");
+    }
+}
