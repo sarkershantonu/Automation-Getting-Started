027bc5d11a9f3988b219fd12ac39f7caf639b4c9
shantonu <sarker.shantonu@gmail.com>
2022-09-27 17:52:34 -0500

    full complete on runner class

--- testng-executable/src/main/java/org/automation/example/runner/TestNgRunner.java
+++ testng-executable/src/main/java/org/automation/example/runner/TestNgRunner.java
@@ -1,4 +1,22 @@
 package org.automation.example.runner;
 
+import org.testng.TestNG;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestNgRunner {
+
+    //AdhocTests.xml
+    private final List<String> suites;
+
+    public TestNgRunner(String... suiteFiles) {
+        suites = new ArrayList<>(Arrays.asList(suiteFiles));
+    }
+    public void run(){
+        TestNG ng = new TestNG();
+        ng.setTestSuites(suites);
+        ng.run();
+    }
 }
