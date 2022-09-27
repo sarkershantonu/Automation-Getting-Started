fa0cb1837782ba5a77aca3e29591db9566e8f1e8
shantonu <sarker.shantonu@gmail.com>
2022-09-27 17:52:28 -0500

    full complete on runner class

--- testng-executable/src/main/java/org/automation/example/Application.java
+++ testng-executable/src/main/java/org/automation/example/Application.java
@@ -1,7 +1,18 @@
 package org.automation.example;
 
-public class Application {
-    public static void main(String[] args) {
+import org.automation.example.runner.TestNgRunner;
 
+import java.io.File;
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        System.out.println(" Application is Running");
+        System.out.println(args.length);
+        File suite = new File("./suites/ExceptionTests.xml");
+        System.out.println(suite.getAbsolutePath());
+        System.out.println(suite.getCanonicalPath());
+        TestNgRunner runner = new TestNgRunner(suite.getCanonicalPath());
+        runner.run();
     }
 }
