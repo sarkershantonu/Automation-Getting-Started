b6ddb0f1bff4e2163bf9c1eda80068c29d0db187
shantonu <sarker.shantonu@gmail.com>
2022-09-26 21:28:41 -0500

    test listener added

--- testng-executable/src/main/java/org/automation/example/tests/CalculatorBaseTest.java
+++ testng-executable/src/main/java/org/automation/example/tests/CalculatorBaseTest.java
@@ -1,8 +1,11 @@
 package org.automation.example.tests;
 
 import org.automation.example.app.Calculator;
+import org.automation.example.listeners.TestLogger;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestLogger.class)
 public abstract class CalculatorBaseTest {
     protected Calculator calculator;
     @BeforeClass
