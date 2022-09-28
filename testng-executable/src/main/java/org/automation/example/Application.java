b6513cd78d87e218bebcf3c85e7a712a89be89b5
shantonu <sarker.shantonu@gmail.com>
2022-09-27 22:26:27 -0500

    executable is running with default config

--- testng-executable/src/main/java/org/automation/example/Application.java
+++ testng-executable/src/main/java/org/automation/example/Application.java
@@ -1,18 +1,36 @@
 package org.automation.example;
 
 import org.automation.example.runner.TestNgRunner;
+import org.automation.example.utils.PropertyFileLoader;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        System.out.println(" Application is Running");
+        System.out.println(" Trying to run test");
         System.out.println(args.length);
-        File suite = new File("./suites/ExceptionTests.xml");
-        System.out.println(suite.getAbsolutePath());
-        System.out.println(suite.getCanonicalPath());
-        TestNgRunner runner = new TestNgRunner(suite.getCanonicalPath());
+        setSystemProperty(new PropertyFileLoader("app.properties").load());
+        String[] suiteNames = {getFilePaths("AdhocTests.xml"),getFilePaths("ExceptionTests.xml")};
+        TestNgRunner runner = new TestNgRunner(suiteNames);
         runner.run();
+    }
+
+    public static String getFilePaths(String suiteName) throws IOException {
+        return new File("./suites/"+suiteName).getCanonicalPath();
+    }
+    public static void setSystemProperty(final Properties properties){
+        for (String key : properties.stringPropertyNames()) {
+            System.setProperty(key, properties.getProperty(key));
+        }
+    }
+    public static void setSystemPropertyKeepingExisting(final Properties properties) {
+        for (String key : properties.stringPropertyNames()) {
+            if((null==System.getProperty(key))||System.getProperty(key)==""||(System.getProperty(key)==" "))
+            {
+                System.setProperty(key, properties.getProperty(key));
+            }
+        }
     }
 }
