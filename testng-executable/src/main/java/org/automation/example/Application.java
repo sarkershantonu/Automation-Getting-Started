8e1ac231f0aa997fc586ce6b7ca2b706a29f3c0e
shantonu <sarker.shantonu@gmail.com>
2022-09-30 23:53:12 -0500

    App running success

--- testng-executable/src/main/java/org/automation/example/Application.java
+++ testng-executable/src/main/java/org/automation/example/Application.java
@@ -1,27 +1,24 @@
 package org.automation.example;
 
 import org.automation.example.runner.TestNgRunner;
-import org.automation.example.config.PropertyFileLoader;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
-import static org.automation.example.config.AppConfig.initiateProperties;
+import static org.automation.example.config.AppConfigHelper.initiateProperties;
+import static org.automation.example.config.SuiteFileLoader.getFilePaths;
 
 public class Application {
     public static void main(String[] args) throws IOException {
         System.out.println(" Trying to run test");
-        System.out.println(args.length);
         initiateProperties();
-        String[] suiteNames = {getFilePaths("AdhocTests.xml"),getFilePaths("ExceptionTests.xml")};
+        String[] suiteNames = {getFilePaths("AdhocTests.xml"), getFilePaths("ExceptionTests.xml")};
+
         TestNgRunner runner = new TestNgRunner(suiteNames);
         runner.run();
     }
 
-    public static String getFilePaths(String suiteName) throws IOException {
-        return new File("./suites/"+suiteName).getCanonicalPath();
-    }
+
 
     public static void setSystemPropertyKeepingExisting(final Properties properties) {
         for (String key : properties.stringPropertyNames()) {
