64b0b87a2a9fa4175016eb9c4815e0c14ddbbec9
shantonu <sarker.shantonu@gmail.com>
2022-10-18 23:56:03 -0500

    adhoc  test added

--- junit5-executable/src/main/java/org/automation/junit5/tests/AdhocTests.java
+++ junit5-executable/src/main/java/org/automation/junit5/tests/AdhocTests.java
@@ -1,4 +1,17 @@
 package org.automation.junit5.tests;
 
-public class AdhocTests extends CalculatorTestBase{
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AdhocTests extends CalculatorJunit5TestBase {
+    @Test
+    public void testIntAdd(){
+        assertEquals(calculator.add(5,3),8,"Addition Fail");
+    }
+
+    @Test
+    public void testFloatAdd(){
+        assertEquals(calculator.add(5.8,3.3),9.1,"Addition Fail");
+    }
 }
