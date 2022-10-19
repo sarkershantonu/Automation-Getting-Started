9a17c5f7ee5a9bdff1940caf1618328bd516a881
shantonu <sarker.shantonu@gmail.com>
2022-10-19 00:22:47 -0500

    logger applied

--- junit5-executable/src/main/java/org/automation/junit5/tests/CalculatorJunit5TestBase.java
+++ junit5-executable/src/main/java/org/automation/junit5/tests/CalculatorJunit5TestBase.java
@@ -1,8 +1,11 @@
 package org.automation.junit5.tests;
 
 import org.automation.junit5.app.Calculator;
+import org.automation.junit5.reporting.TestLoggerExtension;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(TestLoggerExtension.class)
 public abstract class CalculatorJunit5TestBase {
     protected static Calculator calculator;
     @BeforeAll
