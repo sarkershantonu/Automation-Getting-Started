c5e022cd7d1c55db6b92c5cd1b2a9951ba9be5a2
shantonu <sarker.shantonu@gmail.com>
2022-10-18 23:55:54 -0500

    base test added

--- junit5-executable/src/main/java/org/automation/junit5/tests/CalculatorJunit5TestBase.java
+++ junit5-executable/src/main/java/org/automation/junit5/tests/CalculatorJunit5TestBase.java
@@ -3,7 +3,7 @@ package org.automation.junit5.tests;
 import org.automation.junit5.app.Calculator;
 import org.junit.jupiter.api.BeforeAll;
 
-public abstract class CalculatorTestBase {
+public abstract class CalculatorJunit5TestBase {
     protected static Calculator calculator;
     @BeforeAll
     public static void initClass(){
