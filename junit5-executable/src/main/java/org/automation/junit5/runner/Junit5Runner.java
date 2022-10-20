0ea60002385ff1020133bcf616ac7430a50b818f
shantonu <sarker.shantonu@gmail.com>
2022-10-19 23:58:31 -0500

    runner class added

--- junit5-executable/src/main/java/org/automation/junit5/runner/Junit5Runner.java
+++ junit5-executable/src/main/java/org/automation/junit5/runner/Junit5Runner.java
@@ -3,8 +3,17 @@ package org.automation.junit5.runner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Junit5Runner implements Runnable{
-   private Logger logging = LoggerFactory.getLogger(Junit5Runner.class);
+   private static final Logger logging = LoggerFactory.getLogger(Junit5Runner.class);
+   private final List<Class<?>> testClasses;
+
+   public Junit5Runner(List<Class<?>> testClasses) {
+      this.testClasses = testClasses;
+   }
+
 
 
    public void run(){
