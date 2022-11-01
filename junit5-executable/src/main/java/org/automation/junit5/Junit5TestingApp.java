931e99e1d591a6b3308396e2abafd8c2ac170a4c
shantonu <sarker.shantonu@gmail.com>
2022-11-01 00:06:24 -0500

    running ok, done example complete for multi class executable

--- junit5-executable/src/main/java/org/automation/junit5/Junit5TestingApp.java
+++ junit5-executable/src/main/java/org/automation/junit5/Junit5TestingApp.java
@@ -1,5 +1,11 @@
 package org.automation.junit5;
 
+import org.automation.junit5.runner.Junit5Runner;
+import org.automation.junit5.tests.AdhocTests;
+import org.automation.junit5.tests.ExceptionTests;
+
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -14,5 +20,8 @@ public class Junit5TestingApp
 
     public static void runJunit5Tests(){
         System.out.println( "Starting Junit5 Test Runner" );
+
+        Junit5Runner runner = new Junit5Runner(Arrays.asList(new Class[]{AdhocTests.class, ExceptionTests.class}));
+        runner.runAll();
     }
 }
