cc849615a4995db8572b8e107109172300a07b13
shantonu <sarker.shantonu@gmail.com>
2022-11-01 23:54:14 -0500

    refactored with new names

--- junit5-executable/src/main/java/org/automation/junit5/runner/Junit5TestPackageRunner.java
+++ junit5-executable/src/main/java/org/automation/junit5/runner/Junit5TestPackageRunner.java
@@ -1,9 +1,9 @@
 package org.automation.junit5.runner;
 
-public class Junit5PackageRunner {
+public class Junit5TestPackageRunner {
     private final String packageName;
 
-    public Junit5PackageRunner(String packageName) {
+    public Junit5TestPackageRunner(String packageName) {
         this.packageName = packageName;
     }
 }
