a76bc5c4f02ac7b39ea78a03477d2cf55d98f305
shantonu <sarker.shantonu@gmail.com>
2022-11-01 23:54:17 -0500

    refactored with new names

--- junit5-executable/src/main/java/org/automation/junit5/Junit5TestingApp.java
+++ junit5-executable/src/main/java/org/automation/junit5/Junit5TestingApp.java
@@ -1,6 +1,6 @@
 package org.automation.junit5;
 
-import org.automation.junit5.runner.Junit5Runner;
+import org.automation.junit5.runner.Junit5TestClassRunner;
 import org.automation.junit5.tests.AdhocTests;
 import org.automation.junit5.tests.ExceptionTests;
 
@@ -21,7 +21,7 @@ public class Junit5TestingApp
     public static void runJunit5Tests(){
         System.out.println( "Starting Junit5 Test Runner" );
 
-        Junit5Runner runner = new Junit5Runner(Arrays.asList(new Class[]{AdhocTests.class, ExceptionTests.class}));
+        Junit5TestClassRunner runner = new Junit5TestClassRunner(Arrays.asList(new Class[]{AdhocTests.class, ExceptionTests.class}));
         runner.runAll();
     }
 }
