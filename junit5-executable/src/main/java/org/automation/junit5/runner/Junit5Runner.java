2a2f6302fb14a54cc45662e6ae6c7a02331743d0
shantonu <sarker.shantonu@gmail.com>
2022-11-02 00:23:37 -0500

    junit Package runner done : running with logs and pattern

--- junit5-executable/src/main/java/org/automation/junit5/runner/Junit5Runner.java
+++ junit5-executable/src/main/java/org/automation/junit5/runner/Junit5Runner.java
@@ -2,11 +2,13 @@ package org.automation.junit5.runner;
 
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public abstract class Junit5Runner {
-
+    protected static final Logger logging = LoggerFactory.getLogger("CalculatorLogger");
     private List<Class<?>> listeners;
 
     public TestExecutionListener getDefaultListener(){
