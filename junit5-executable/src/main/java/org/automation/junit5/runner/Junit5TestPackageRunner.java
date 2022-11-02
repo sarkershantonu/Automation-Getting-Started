287af728982233b1d1fdcb50eb577a3a9ed04b8a
shantonu <sarker.shantonu@gmail.com>
2022-11-02 00:10:19 -0500

    refactored with new array support

--- junit5-executable/src/main/java/org/automation/junit5/runner/Junit5TestPackageRunner.java
+++ junit5-executable/src/main/java/org/automation/junit5/runner/Junit5TestPackageRunner.java
@@ -1,9 +1,28 @@
 package org.automation.junit5.runner;
 
-public class Junit5TestPackageRunner {
+import org.junit.platform.engine.discovery.ClassNameFilter;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+
+
+public class Junit5TestPackageRunner extends Junit5Runner implements Runnable {
     private final String packageName;
 
     public Junit5TestPackageRunner(String packageName) {
         this.packageName = packageName;
+    }
+    public void run(){
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors( DiscoverySelectors.selectPackage(packageName))
+                .filters(ClassNameFilter.includeClassNamePatterns("*Test*")
+        ).build();
+        Launcher launcher = LauncherFactory.create();
+        TestPlan plan = launcher.discover(request);
+        launcher.registerTestExecutionListeners();
+        launcher.execute(request);
     }
 }
