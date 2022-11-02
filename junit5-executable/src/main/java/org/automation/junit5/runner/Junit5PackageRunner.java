da652c6556bc2ef35a240578a96c5de42ebdf4b0
shantonu <sarker.shantonu@gmail.com>
2022-11-01 23:53:19 -0500

    package runner

---
+++ junit5-executable/src/main/java/org/automation/junit5/runner/Junit5PackageRunner.java
@@ -0,0 +1,9 @@
+package org.automation.junit5.runner;
+
+public class Junit5PackageRunner {
+    private final String packageName;
+
+    public Junit5PackageRunner(String packageName) {
+        this.packageName = packageName;
+    }
+}
