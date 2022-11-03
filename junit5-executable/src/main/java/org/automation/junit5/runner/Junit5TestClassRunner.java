2777f5e037c372e3f3c44929d1754b36e468c09d
shantonu <sarker.shantonu@gmail.com>
2022-11-03 07:09:35 -0500

    Adding configurations for runner app

--- junit5-executable/src/main/java/org/automation/junit5/runner/Junit5TestClassRunner.java
+++ junit5-executable/src/main/java/org/automation/junit5/runner/Junit5TestClassRunner.java
@@ -5,8 +5,6 @@ import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.TestPlan;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
@@ -23,7 +21,7 @@ public class Junit5TestClassRunner extends Junit5Runner implements Runnable {
     }
 
     public void run() {
-        logging.info("Starting the runner");
+        logger.info("Starting the runner");
         if(classes.size()==1){
             singleClassRunner(classes.get(0));
         }
@@ -34,7 +32,7 @@ public class Junit5TestClassRunner extends Junit5Runner implements Runnable {
     }
 
     public void singleClassRunner(Class<?> kLass) {
-        logging.debug("Running Single Class : "+kLass.getCanonicalName());
+        logger.debug("Running Single Class : "+kLass.getCanonicalName());
         LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request().selectors(selectClass(kLass)).build();
         Launcher launcher = LauncherFactory.create();
         TestPlan testPlan = launcher.discover(request);
@@ -43,7 +41,7 @@ public class Junit5TestClassRunner extends Junit5Runner implements Runnable {
     }
 
     public void multiClassRunner(){
-        logging.info("Running Multiple Classes");
+        logger.info("Running Multiple Classes");
         for(Class<?> aClass : classes){
             singleClassRunner(aClass);
         }
