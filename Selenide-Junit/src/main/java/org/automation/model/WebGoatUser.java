b10b1b8629899a312ab5b216b084764a168b5ecc
shantonu <sarker.shantonu@gmail.com>
2024-12-03 23:58:17 -0600

    implement selenide config

---
+++ Selenide-Junit/src/main/java/org/automation/model/WebGoatUser.java
@@ -0,0 +1,22 @@
+package org.automation.model;
+
+public class WebGoatUser {
+    private String user;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
