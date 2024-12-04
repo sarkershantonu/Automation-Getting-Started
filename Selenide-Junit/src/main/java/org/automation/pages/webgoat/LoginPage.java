03e98010024a0e35920d624b49d2ad0e17cfb7f7
shantonu <sarker.shantonu@gmail.com>
2024-12-03 23:58:11 -0600

    implement selenide config

--- Selenide-Junit/src/main/java/org/automation/pages/webgoat/LoginPage.java
+++ Selenide-Junit/src/main/java/org/automation/pages/webgoat/LoginPage.java
@@ -1,6 +1,17 @@
 package org.automation.pages.webgoat;
 
+import org.automation.core.WebGoatConfig;
 import org.automation.pages.PageBase;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class LoginPage extends PageBase {
+    public static String path ="/WebGoat/login";
+
+    public LoginPage load(){
+        open(new WebGoatConfig());
+        return this;
+    }
+
 }
