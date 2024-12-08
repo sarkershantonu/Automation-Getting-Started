4a95add35e9b9990b057a40464aa6c8975fb36d6
shantonu <sarker.shantonu@gmail.com>
2024-12-08 01:48:26 -0600

    todo : selenide property complete integration .. just adde property names

--- Selenide-Junit/src/main/java/org/automation/pages/webgoat/LoginPage.java
+++ Selenide-Junit/src/main/java/org/automation/pages/webgoat/LoginPage.java
@@ -1,17 +1,15 @@
 package org.automation.pages.webgoat;
 
-import com.codeborne.selenide.SelenideConfig;
 import org.automation.core.WebGoatConfig;
 import org.automation.pages.PageBase;
 
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class LoginPage extends PageBase {
     public static String path ="/WebGoat/login";
 
     public LoginPage load(){
-        open(WebGoatConfig);
+        //open(WebGoatConfig);
         open(WebGoatConfig.BASE_URL+path);
         return this;
     }
