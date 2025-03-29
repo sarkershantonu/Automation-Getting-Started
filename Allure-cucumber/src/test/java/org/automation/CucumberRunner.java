ee0e1f4467d325ede0021e7d76f5113dee9f7bfc
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-03-28 23:52:33 -0500

    fixing pom for junit 4

--- Allure-cucumber/src/test/java/org/automation/CucumberRunner.java
+++ Allure-cucumber/src/test/java/org/automation/CucumberRunner.java
@@ -1,15 +1,22 @@
 package org.automation;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
+/*import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;*/
+
+import org.junit.Test;
 
 /**
  * Created by shantonu on 5/9/16.
  */
-@RunWith(Cucumber.class)
+/*@RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumberReport","json:target/cucumberReport/cucumber.json" }
         , features = {"src/test/resources/org/automation/calculator.feature"}
-        , glue = {""})
+        , glue = {""})*/
+@Test
+@CucumberOptions(
+        plugin = {
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        }
+)
 public class CucumberRunner {
 }
