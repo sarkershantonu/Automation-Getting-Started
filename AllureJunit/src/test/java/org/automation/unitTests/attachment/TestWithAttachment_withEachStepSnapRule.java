b19ef27e42b2115fb1415ef2989efe55618ea7e2
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-03 00:42:11 -0500

    primary assist done : need to change annotation implementations

--- AllureJunit/src/test/java/org/automation/unitTests/attachment/TestWithAttachment_withEachStepSnapRule.java
+++ AllureJunit/src/test/java/org/automation/unitTests/attachment/TestWithAttachment_withEachStepSnapRule.java
@@ -1,24 +1,25 @@
 package org.automation.unitTests.attachment;
 
+import io.qameta.allure.Description;
 import org.automation.core.TestRule_ScreenShotOnEachStep;
 import org.automation.core.TestingCalculator;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestWatcher;
-import ru.yandex.qatools.allure.annotations.Title;
 
 /**
  * Created by shantonu on 9/11/16.
  */
 
-@Title("Attachment as text")
+
 public class TestWithAttachment_withEachStepSnapRule extends TestingCalculator {
 
     @Rule
     public TestWatcher screenShotInEachStep = new TestRule_ScreenShotOnEachStep();
 
     @Test
+    @Description("Attachment as text")
     public void testAdditionFail() {
         Assert.assertEquals(35.0, aCalculator.add(10.0, 15.0), 0.01);
         attachThisMessage("https://www.google.pl");
