0af16ae58f71f972f314121b0f454fda5495a271
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-05 00:13:09 -0500

    @Stories   fixed

--- AllureJunit/src/test/java/org/automation/unitTests/TestWithStories.java
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithStories.java
@@ -1,25 +1,23 @@
 package org.automation.unitTests;
 
+import io.qameta.allure.Stories;
+import io.qameta.allure.Story;
 import org.automation.core.TestingCalculator;
 import org.junit.Assert;
 import org.junit.Test;
-import ru.yandex.qatools.allure.annotations.Description;
-import ru.yandex.qatools.allure.annotations.Features;
-import ru.yandex.qatools.allure.annotations.Stories;
 
-/**
- * Created by shantonu on 9/11/16.
+/*** Created by shantonu on 9/11/16.
  */
-@Stories(value = {"Class Story : An user should be able to calculate with two numbers",
-        "Class Story : An user should be able to add & divide"})
+@Stories({@Story("Class Story : An user should be able to calculate with two numbers"),
+        @Story("Class Story : An user should be able to add & divide")})
 public class TestWithStories extends TestingCalculator {
     @Test
-    @Stories(value = "Method Story : An user should be able to add two numbers")
+    @Story(value = "Method Story : An user should be able to add two numbers")
     public void testAddition() {
         Assert.assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
     }
     @Test
-    @Stories("Method Story : An user should be able to divide two numbers ")
+    @Story("Method Story : An user should be able to divide two numbers ")
     public void testDiv(){
         Assert.assertEquals(5.0, aCalculator.div(250.0,50.0), 0.01);
     }
