1d9a3cbdef1cfb93ad0583785447fea854a0437b
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-05 00:09:59 -0500

    @@Feature fixed

--- AllureJunit/src/test/java/org/automation/unitTests/TestWithFeatures.java
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithFeatures.java
@@ -1,23 +1,23 @@
 package org.automation.unitTests;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Features;
 import org.automation.core.TestingCalculator;
 import org.junit.Assert;
 import org.junit.Test;
-import ru.yandex.qatools.allure.annotations.Description;
-import ru.yandex.qatools.allure.annotations.Features;
 
 /**
  * Created by shantonu on 9/11/16.
  */
-@Features(value = {"Class Feature : Add","Class Feature : divide"})
+@Features( {@Feature("Class Feature : Add"),@Feature("Class Feature : divide")})
 public class TestWithFeatures extends TestingCalculator{
     @Test
-    @Features("Method Features : Addition")
+    @Feature("Method Features : Addition")
     public void testAddition() {
         Assert.assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
     }
     @Test
-    @Features("Method Features : division ")
+    @Feature("Method Features : division ")
     public void testDiv(){
         Assert.assertEquals(5.0, aCalculator.div(250.0,50.0), 0.01);
     }
