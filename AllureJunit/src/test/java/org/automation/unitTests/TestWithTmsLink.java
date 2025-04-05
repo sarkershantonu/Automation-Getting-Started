19a829a74c9dce9447b16ed5590a1a6993740db1
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-04 23:56:58 -0500

    TMS link updated , replacing @TestCaseId

--- AllureJunit/src/test/java/org/automation/unitTests/TestWithTmsLink.java
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithTmsLink.java
@@ -1,38 +1,36 @@
 package org.automation.unitTests;
 
+import io.qameta.allure.TmsLink;
 import org.automation.core.TestingCalculator;
 import org.junit.Assert;
 import org.junit.Test;
-import ru.yandex.qatools.allure.annotations.Severity;
-import ru.yandex.qatools.allure.annotations.TestCaseId;
-import ru.yandex.qatools.allure.model.SeverityLevel;
 
 /**
  * Created by shantonu on 9/11/16.
  */
-public class TestWithTestCaseId  extends TestingCalculator{
+public class TestWithTmsLink extends TestingCalculator{
     @Test
-    @TestCaseId(value = "ID#001")
+    @TmsLink(value = "ID#001")
     public void testAddition() {
         Assert.assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
     }
     @Test
-    @TestCaseId(value = "ID#002")
+    @TmsLink(value = "ID#002")
     public void testSub(){//fail example
         Assert.assertEquals(25.0, aCalculator.sub(10.0,15.0), 0.01);
     }
     @Test
-    @TestCaseId(value = "ID#003")
+    @TmsLink(value = "ID#003")
     public void testMul(){
         Assert.assertEquals(250.0, aCalculator.mul(10.0,25.0), 0.01);
     }
     @Test
-    @TestCaseId(value = "ID#004")
+    @TmsLink(value = "ID#004")
     public void testDiv(){
         Assert.assertEquals(5.0, aCalculator.div(250.0,50.0), 0.01);
     }
     @Test
-    @TestCaseId(value = "ID#005")
+    @TmsLink(value = "ID#005")
     public void testReminder(){
         Assert.assertEquals(1.0, aCalculator.mod(25.0,3.0), 0.01);
     }
