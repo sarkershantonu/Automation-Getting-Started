a8a5ec70598f5da13e4a954bc0978dafe3074ffb
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-05 00:08:52 -0500

    @Description fixed

--- AllureJunit/src/test/java/org/automation/unitTests/TestWithDescription.java
+++ AllureJunit/src/test/java/org/automation/unitTests/TestWithDescription.java
@@ -1,16 +1,13 @@
 package org.automation.unitTests;
 
+import io.qameta.allure.Description;
 import org.automation.core.TestingCalculator;
 import org.junit.Assert;
 import org.junit.Test;
-import ru.yandex.qatools.allure.annotations.Description;
-import ru.yandex.qatools.allure.annotations.Severity;
-import ru.yandex.qatools.allure.model.SeverityLevel;
-
 /**
  * Created by shantonu on 9/11/16.
  */
-@Description("Class Description : This will test calculator")
+
 public class TestWithDescription extends TestingCalculator {
     @Test
     @Description("Method Description : This will test addidtion")
