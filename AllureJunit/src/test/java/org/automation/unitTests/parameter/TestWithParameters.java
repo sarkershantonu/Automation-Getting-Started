fb154d082607e62872c8ea2f304837b77a64c108
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-06 22:40:40 -0500

    Parameterized Tests

--- AllureJunit/src/test/java/org/automation/unitTests/parameter/TestWithParameters.java
+++ AllureJunit/src/test/java/org/automation/unitTests/parameter/TestWithParameters.java
@@ -2,7 +2,6 @@ package org.automation.unitTests.parameter;
 
 
 import org.automation.core.TestingCalculator;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,41 +14,37 @@ import java.util.Collection;
 
 /**
  * The difference in this case with single @Parameter using is, no constructor needed but those fields need to be public
+ *
  * @author shantonu
- *=> Allure @Parameter does not works, we can not see in tests about parameters
- *
+ * => Allure @Parameter does not works, we can not see in tests about parameters
+ * <p>
  * because it is not initiated. it works when it is initiated either in method or in constructors. (see constructor parametrized tests)
- *
- *
+ * updated on Apr 6, 25
  */
 @RunWith(Parameterized.class)
-public class TestWithParameters extends TestingCalculator{
+public class TestWithParameters extends TestingCalculator {
+    @Parameter(0)
+    public int expected;
 
-	@Parameter("Expected")
-	@Parameter(value=0)
-	public int expected;
+    @Parameter(1)
+    public int first;
 
-	@ru.yandex.qatools.allure.annotations.Parameter("First")
-	@Parameter(value=1)
-	public int first;
-	@ru.yandex.qatools.allure.annotations.Parameter("Second")
-	@Parameter(value=2)
-	public int second;
+    @Parameter(2)
+    public int second;
 
-	
-	
-	@Parameters(name = "Test : {index} : add({1}+{2})= Expecting {0}")//name will be shared among all tests
-	public static Collection addNumbers() {
-		return Arrays.asList(new Integer[][] { { 3, 2, 1 }, { 5, 2, 3 }, { 9, 8, 1 }, { 200, 50, 150 } });
-	}
-	@Test
-	public void testAddWithParameters() {
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		Assert.assertEquals(expected, aCalculator.add(first, second),0.01);
-	}
-	
+    @Parameters(name = "Test : {index} : add({1}+{2})= Expecting {0}")//name will be shared among all tests
+    public static Collection addNumbers() {
+        return Arrays.asList(new Integer[][]{{3, 2, 1}, {5, 2, 3}, {9, 8, 1}, {200, 50, 150}});
+    }
+
+    @Test
+    public void testAddWithParameters() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(expected, aCalculator.add(first, second), 0.01);
+    }
+
 }
