5e4f95a9256b29409cffd6deb8688e9f14859339
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-06 22:38:25 -0500

    zohhak removed

--- AllureJunit/src/test/java/org/automation/unitTests/parameter/TestWithParameters_constructor.java
+++ AllureJunit/src/test/java/org/automation/unitTests/parameter/TestWithParameters_constructor.java
@@ -2,7 +2,6 @@ package org.automation.unitTests.parameter;
 
 import org.automation.core.TestingCalculator;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
