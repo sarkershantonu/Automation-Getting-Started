dd12694329c9c44377e23c377cb39589e2e74383
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-13 04:51:24 -0500

    project refactored : after update , Parameterized tests need to fix, getting null values for test.

--- Allure2-Junit5/src/test/java/org/automation/junit5/core/StringUtil.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/core/StringUtil.java
@@ -2,6 +2,6 @@ package org.automation.junit5.core;
 
 public class StringUtil {
     public static boolean isBlank(final String input){
-        return null==input||input.trim().isEmpty();
+        return (null==input)||(input.trim().isEmpty());
     }
 }
