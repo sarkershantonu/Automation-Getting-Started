4643172a20ea21be31d9b656955761930220b3b1
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-24 00:21:05 -0500

    value source example complete

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/parameterized/TestCustomValueSources.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/parameterized/TestCustomValueSources.java
@@ -1,6 +1,6 @@
 package org.automation.junit5.tests.parameterized;
 
-import org.automation.junit5.support.MyFloatDataValueSource;
+import org.automation.junit5.support.*;
 import org.junit.jupiter.params.ParameterizedTest;
 
 public class TestCustomValueSources {
@@ -9,4 +9,43 @@ public class TestCustomValueSources {
     public void testFloatValueSource(float input){
         System.out.println(input);
     }
+    @ParameterizedTest
+    @MyBooleanDataValueSource
+    public void testBooleanValueSource(boolean input){
+        System.out.println(input);
+    }
+
+    @ParameterizedTest
+    @MyLongDataValueSource
+    public void testLongValueSource(long input){
+        System.out.println(input);
+    }
+
+    @ParameterizedTest
+    @MyShortDataValueSource
+    public void testShortValueSource(short input){
+        System.out.println(input);
+    }
+    @ParameterizedTest
+    @MyCharDataValueSource
+    public void testCharValueSource(char input){
+        System.out.println( input);
+    }
+    @ParameterizedTest
+    @MyDoubleDataValueSource
+    public void testDoubleValueSource(double input){
+        System.out.println( input);
+    }
+    @ParameterizedTest
+    @MyIntDataValueSource
+    public void testIntValueSource(int input){
+        System.out.println( input);
+    }
+    @ParameterizedTest
+    @MyClassAsValueSource
+    public void testClassValueSource(Class<?> input){
+        System.out.println( input.getDeclaredFields()[0]);//first field , usually ID
+    }
+
+
 }
