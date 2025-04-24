70438e6eaab315df4a1665187bfbafe115e5db75
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-24 00:20:59 -0500

    value source example complete

--- Allure2-Junit5/src/test/java/org/automation/junit5/support/data/ClassA.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/support/data/ClassA.java
@@ -1,4 +1,22 @@
 package org.automation.junit5.support.data;
 
 public class ClassA {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
