487d75bc22174e1d500f427dd3c699c7673d6850
Shantonu Sarker <sarker.shantonu@gmail.com>
2025-04-25 23:55:39 -0500

    value source example added

--- Allure2-Junit5/src/test/java/org/automation/junit5/tests/parameterized/TestCustomValueSources.java
+++ Allure2-Junit5/src/test/java/org/automation/junit5/tests/parameterized/TestCustomValueSources.java
@@ -6,45 +6,50 @@ import org.junit.jupiter.params.ParameterizedTest;
 public class TestCustomValueSources {
     @ParameterizedTest
     @MyFloatDataValueSource
-    public void testFloatValueSource(float input){
+    public void testFloatValueSource(float input) {
         System.out.println(input);
     }
+
     @ParameterizedTest
     @MyBooleanDataValueSource
-    public void testBooleanValueSource(boolean input){
+    public void testBooleanValueSource(boolean input) {
         System.out.println(input);
     }
 
     @ParameterizedTest
     @MyLongDataValueSource
-    public void testLongValueSource(long input){
+    public void testLongValueSource(long input) {
         System.out.println(input);
     }
 
     @ParameterizedTest
     @MyShortDataValueSource
-    public void testShortValueSource(short input){
+    public void testShortValueSource(short input) {
         System.out.println(input);
     }
+
     @ParameterizedTest
     @MyCharDataValueSource
-    public void testCharValueSource(char input){
-        System.out.println( input);
+    public void testCharValueSource(char input) {
+        System.out.println(input);
     }
+
     @ParameterizedTest
     @MyDoubleDataValueSource
-    public void testDoubleValueSource(double input){
-        System.out.println( input);
+    public void testDoubleValueSource(double input) {
+        System.out.println(input);
     }
+
     @ParameterizedTest
     @MyIntDataValueSource
-    public void testIntValueSource(int input){
-        System.out.println( input);
+    public void testIntValueSource(int input) {
+        System.out.println(input);
     }
+
     @ParameterizedTest
     @MyClassAsValueSource
-    public void testClassValueSource(Class<?> input){
-        System.out.println( input.getDeclaredFields()[0]);//first field , usually ID
+    public void testClassValueSource(Class<?> input) {
+        System.out.println(input.getDeclaredFields()[0]);//first field , usually ID
     }
 
 
