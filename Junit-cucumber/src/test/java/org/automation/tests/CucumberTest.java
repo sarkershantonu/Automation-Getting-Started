e12fedda1cfe8ab1902a1190b3b4d2282db29ef6
shantonu sarker <sarker.shantonu@gmail.com>
2025-04-29 02:08:40 -0500

    only added required plugins

--- Junit-cucumber/src/test/java/org/automation/tests/CucumberTest.java
+++ Junit-cucumber/src/test/java/org/automation/tests/CucumberTest.java
@@ -1,15 +1,19 @@
 package org.automation.tests;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 /**
  * Created by shantonu on 5/9/16.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumberReport","json:target/cucumber.json" }
+@CucumberOptions(plugin = {"pretty",
+        "html:target/cucumberReport",
+        "json:target/cucumber.json" }
         , features = {"src/test/resources/features"}
         , glue = {""})
+
 public class CucumberTest {
 }
