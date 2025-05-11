79a21a8860c5dca54928941f89058edcc09fac4a
shantonu sarker <sarker.shantonu@gmail.com>
2025-05-11 00:54:21 -0500

    test fixed, todo behaviour perimeterize

--- ChromeHeadless-Selenium/src/test/java/automation/TestChromeArgConstants.java
+++ ChromeHeadless-Selenium/src/test/java/automation/TestChromeArgConstants.java
@@ -1,39 +1,34 @@
 package automation;
 
 import org.automation.ChromeManager;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
+
+import static org.automation.ChromeManager.destroyBrowser;
 
 public class TestChromeArgConstants {
 
     WebDriver driver;
 
-    @Before public void init(){
-/*        System.setProperty("webdriver.chrome.driver","/home/shantonu/env/chromedriver");
-        System.setProperty("chrome.log.path","./logs/chrome.log");
-        System.setProperty("linux.chrome.bin", "/usr/bin/google-chrome");*/
+    @BeforeClass
+    public static void init(){
         System.setProperty("chrome.verbose", "true");
-
     }
 
     @Test
-    public void testChrome() {
+    public void testChromeWithUI() {
         driver = ChromeManager.getBrowser();
         driver.get("https://www.linkedin.com");
+        Assert.assertEquals("LinkedIn: Log In or Sign Up",driver.getTitle());
     }
 
     @Test public void testChromeHeadless() throws InterruptedException {
         driver = ChromeManager.getBrowser();
         driver.get("https://www.facebook.com");
-        Assert.assertEquals("Facebook - Log In or Sign Up",driver.getTitle());
-
-        Thread.sleep(10000);
+        Assert.assertEquals("Facebook - log in or sign up",driver.getTitle());
     }
 
     @After public void end(){
-        driver.close();
+        destroyBrowser();
     }
 }
