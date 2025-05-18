37ff93f79f2aef82b1931b221394e04f7c793913
shantonu sarker <sarker.shantonu@gmail.com>
2025-05-18 00:23:20 -0500

    cucumber -> update , need to fix generated class template

--- Cucumber-Parallel/src/test/java/org/automation/CucumberRunner.java
+++ Cucumber-Parallel/src/test/java/org/automation/CucumberRunner.java
@@ -1,7 +1,8 @@
 package org.automation;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 /**
